7494088598f608b8588224cc57b5fbee86bff874
James Mba Azam <jamesazam@sun.ac.za>
2019-02-21 16:55:36 +0200

    major: app is now able to collect site information and populate the table.

--- ORV_coldchain_costs_app/ORV_coldchain_costs_app.R
+++ ORV_coldchain_costs_app/ORV_coldchain_costs_app.R
@@ -1,14 +1,6 @@
 library(shiny)
 library(dplyr)
 
-# data
-site_info_table <- tibble(
-  dist_from_base = as.numeric(),
-  near_pop = as.numeric(),
-  far_pop = as.numeric(),
-  team_days_fixed = as.numeric(),
-  team_days_mobile = as.numeric()
-)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -40,21 +32,22 @@ ui <- fluidPage(
           tabsetPanel(
             tabPanel(title = "Table of sites added", tableOutput("all_sites")),
             tabPanel(title = "Strategy comparison plots", plotOutput("plot"))
-          ), 
-          
-          hr(), #horizontal rule
-          tabsetPanel(
-            #    title = "Individual strategy results",
-            tabPanel(
-              title = "Monodose FCC",
-              tags$h4("Initial volume of ice required for transport"),
-              wellPanel(htmlOutput("ice_vol_init_monodoseFCC")),
-              tags$h4("Quantity of ice packs required"),
-              wellPanel(htmlOutput("ice_packs_required_monodoseFCC")),
-              tags$h4("Time needed to freeze initial ice"),
-              wellPanel(htmlOutput("Init_ice_freezeTime_monodoseFCC"))
-            )
           )
+          # , 
+          # 
+          # hr(), #horizontal rule
+          # tabsetPanel(
+          #   #    title = "Individual strategy results",
+          #   tabPanel(
+          #     title = "Monodose FCC",
+          #     tags$h4("Initial volume of ice required for transport"),
+          #     wellPanel(htmlOutput("ice_vol_init_monodoseFCC")),
+          #     tags$h4("Quantity of ice packs required"),
+          #     wellPanel(htmlOutput("ice_packs_required_monodoseFCC")),
+          #     tags$h4("Time needed to freeze initial ice"),
+          #     wellPanel(htmlOutput("Init_ice_freezeTime_monodoseFCC"))
+          #   )
+          # )
         
       
     ) # main panel closing bracket
@@ -62,8 +55,19 @@ ui <- fluidPage(
 )
 # Define server logic required to draw a histogram
 server <- function(input, output, session) {
-  observeEvent(input$add_site_info, {
-    showModal(modalDialog(
+  #create dataframe of new site for updating the old table
+  site_new <- reactiveValues(data = NULL)
+  
+  # empty data frame for storing site information
+  site_table <- reactiveValues( data = tibble(
+    dist_from_base = as.numeric(),
+    near_pop = as.numeric(),
+    far_pop = as.numeric(),
+    team_days_fixed = as.numeric(),
+    team_days_mobile = as.numeric()
+  ))
+
+   collect_site_info <- function(){modalDialog(
       title = "Site information",
       numericInput("dist_from_base_new", "How far is site from base?", value = 0, min = 0, step = 0.01),
       numericInput("near_pop_new", "How many people are near?", value = 0, min = 0, step = 1),
@@ -75,45 +79,32 @@ server <- function(input, output, session) {
         modalButton("Dismiss"),
         actionButton("add_site", "Add site")
       )
-    ))
-    # site_new <- tibble(
-    #   dist_from_base = input$dist_from_base_new,
-    #   near_pop = input$near_pop_new,
-    #   far_pop = input$far_pop_new,
-    #   team_days_fixed = input$team_days_fixed_new,
-    #   team_days_mobile = input$team_days_mobile_new
-    # )
-    # 
-    # observeEvent(input$add_site, {
-    #   site_info_table <- bind_rows(site_info_table, site_new)
-    # })
-  })
-
-  # When user clicks the add site button, create a new tibble with the info they'll enter and bind that to the existing data frame
-  observeEvent(input$add_site, {
-    site_new <- tibble(
-      dist_from_base = input$dist_from_base_new,
-      near_pop = input$near_pop_new,
-      far_pop = input$far_pop_new,
-      team_days_fixed = input$team_days_fixed_new,
-      team_days_mobile = input$team_days_mobile_new
     )
-    site_info_table <- bind_rows(site_info_table, site_new)
+   }
+
+
+observeEvent(input$add_site_info, {
+  showModal(collect_site_info()) 
+})  
+
+
+# When user clicks the add site button, update the old data frame with the new information
+observeEvent(input$add_site, {
+      site_table$data = bind_rows(site_table$data, tibble(
+        dist_from_base = input$dist_from_base_new,
+        near_pop = input$near_pop_new,
+        far_pop = input$far_pop_new,
+        team_days_fixed = input$team_days_fixed_new,
+        team_days_mobile = input$team_days_mobile_new
+        ))
+   # site_info_table <- bind_rows(site_info_table, site_new$data)
+    removeModal()
   })
 
-  # site_form <- reactive({
-  #   site_new <- tibble(
-  #     dist_from_base = input$dist_from_base_new,
-  #     near_pop = input$near_pop_new,
-  #     far_pop = input$far_pop_new,
-  #     team_days_fixed = input$team_days_fixed_new,
-  #     team_days_mobile = input$team_days_mobile_new
-  #   )
-  # })
 
 
   # Outputs
-  output$all_sites <- renderTable(site_info_table)
+  output$all_sites <- renderTable(site_table$data)
 }
 
 
