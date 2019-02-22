2b5bcb2f265ebcd8eca89067c50f28d7ffbb1ca4
James Mba Azam <jamesazam@sun.ac.za>
2019-02-22 12:03:30 +0200

    restructured the code and removed unneccesary chunks

--- ORV_coldchain_costs_app/ORV_coldchain_costs_app.R
+++ ORV_coldchain_costs_app/ORV_coldchain_costs_app.R
@@ -29,45 +29,45 @@ ui <- fluidPage(
 
 
     mainPanel(
-          tabsetPanel(
-            tabPanel(title = "Table of sites added", tableOutput("all_sites")),
-            tabPanel(title = "Strategy comparison plots", plotOutput("plot"))
-          )
-          # , 
-          # 
-          # hr(), #horizontal rule
-          # tabsetPanel(
-          #   #    title = "Individual strategy results",
-          #   tabPanel(
-          #     title = "Monodose FCC",
-          #     tags$h4("Initial volume of ice required for transport"),
-          #     wellPanel(htmlOutput("ice_vol_init_monodoseFCC")),
-          #     tags$h4("Quantity of ice packs required"),
-          #     wellPanel(htmlOutput("ice_packs_required_monodoseFCC")),
-          #     tags$h4("Time needed to freeze initial ice"),
-          #     wellPanel(htmlOutput("Init_ice_freezeTime_monodoseFCC"))
-          #   )
-          # )
-        
-      
+      tabsetPanel(
+        tabPanel(title = "Table of sites added", tableOutput("all_sites")),
+        tabPanel(title = "Strategy comparison plots", plotOutput("plot"))
+      )
+      # ,
+      #
+      # hr(), #horizontal rule
+      # tabsetPanel(
+      #   #    title = "Individual strategy results",
+      #   tabPanel(
+      #     title = "Monodose FCC",
+      #     tags$h4("Initial volume of ice required for transport"),
+      #     wellPanel(htmlOutput("ice_vol_init_monodoseFCC")),
+      #     tags$h4("Quantity of ice packs required"),
+      #     wellPanel(htmlOutput("ice_packs_required_monodoseFCC")),
+      #     tags$h4("Time needed to freeze initial ice"),
+      #     wellPanel(htmlOutput("Init_ice_freezeTime_monodoseFCC"))
+      #   )
+      # )
     ) # main panel closing bracket
   )
 )
-# Define server logic required to draw a histogram
-server <- function(input, output, session) {
-  #create dataframe of new site for updating the old table
-  site_new <- reactiveValues(data = NULL)
-  
-  # empty data frame for storing site information
-  site_table <- reactiveValues( data = tibble(
-    dist_from_base = as.numeric(),
-    near_pop = as.numeric(),
-    far_pop = as.numeric(),
-    team_days_fixed = as.numeric(),
-    team_days_mobile = as.numeric()
-  ))
 
-   collect_site_info <- function(){modalDialog(
+
+
+server <- function(input, output, session) {
+
+  # empty data frame for storing all the sites added
+  site_table <- reactiveValues(data = NULL)
+
+  # empty dataframe for temporarily storing the newly added site for updating the old table
+  site_new <- reactiveValues(data = NULL)
+
+
+
+
+  # dialog box to collect site information
+  observeEvent(input$add_site_info, {
+    showModal(modalDialog(
       title = "Site information",
       numericInput("dist_from_base_new", "How far is site from base?", value = 0, min = 0, step = 0.01),
       numericInput("near_pop_new", "How many people are near?", value = 0, min = 0, step = 1),
@@ -79,32 +79,27 @@ server <- function(input, output, session) {
         modalButton("Dismiss"),
         actionButton("add_site", "Add site")
       )
+    ))
+  })
+
+
+  # When user clicks the add site button, update the old data frame with the new information
+  observeEvent(input$add_site, {
+    site_new$data <- tibble(
+      dist_from_base = input$dist_from_base_new,
+      near_pop = input$near_pop_new,
+      far_pop = input$far_pop_new,
+      team_days_fixed = input$team_days_fixed_new,
+      team_days_mobile = input$team_days_mobile_new
     )
-   }
-
-
-observeEvent(input$add_site_info, {
-  showModal(collect_site_info()) 
-})  
-
-
-# When user clicks the add site button, update the old data frame with the new information
-observeEvent(input$add_site, {
-  site_new$data <- tibble(
-    dist_from_base = input$dist_from_base_new,
-    near_pop = input$near_pop_new,
-    far_pop = input$far_pop_new,
-    team_days_fixed = input$team_days_fixed_new,
-    team_days_mobile = input$team_days_mobile_new
-  )
-site_table$data = bind_rows(site_table$data, site_new$data)
-   # site_info_table <- bind_rows(site_info_table, site_new$data)
+    site_table$data <- bind_rows(site_table$data, site_new$data)
+    # site_info_table <- bind_rows(site_info_table, site_new$data)
     removeModal()
   })
 
 
 
-  # Outputs
+  # Output the table of all sites added
   output$all_sites <- renderTable(site_table$data)
 }
 
