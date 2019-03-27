61b5f389503279e98fb09f582293084c258079aa
James Mba Azam <jamesazam@sun.ac.za>
2019-03-27 11:56:23 +0200

    added a UI item to help remove sites

--- ORV_coldchain_costs_app/ORV_coldchain_costs_app.R
+++ ORV_coldchain_costs_app/ORV_coldchain_costs_app.R
@@ -37,6 +37,12 @@ ui <- fluidPage(
       
       br(),
       
+      actionButton("remove_site", "Click to remove a site"),
+      
+      br(),
+      
+      br(),
+      
       selectInput("temp", "Ambient temperature", choices = c("below 40", "above 40")),
       
       br(),
@@ -195,6 +201,35 @@ server <- function(input, output, session) {
       removeModal()
     }
     output$all_sites <- DT::renderDT(site_table$added_sites)
+  })
+  
+  # dialog box to remove sites
+  observeEvent(input$remove_site, {
+    showModal(modalDialog(
+      title = "Enter site row number",
+      numericInput("site_to_remove", "Which site do you want to remove?", value = 0, min = 0, step = 1),
+      fade = TRUE,
+      footer = tagList(
+        modalButton("Dismiss"),
+        actionButton("remove_site_button_click", "Remove site")
+      )
+    ))
+  })
+  
+  observeEvent(input$remove_site_button_click, {
+    if(input$site_to_remove == 0 | input$site_to_remove > dim(site_table$added_sites)[1]){
+      showModal(
+        modalDialog(
+          title = "Wrong input! No site will be removed")
+      ) 
+    }else{
+      showModal(
+        modalDialog(
+          title = paste('Site', input$site_to_remove, 'will be removed and site table will be updated'))
+      ) 
+      site_table$added_sites <- dplyr::slice(site_table$added_sites, -input$site_to_remove)
+      output$all_sites <- DT::renderDT(site_table$added_sites)
+    }
   })
   
   
