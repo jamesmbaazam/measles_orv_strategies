d48b6544f721b1cadcd43e51f55c5d6d4bf6457f
James Mba Azam <jamesazam@sun.ac.za>
2019-03-29 15:55:43 +0200

    script for comparing the mobile team days between the monodose for far campaigns OCC and 10 dose for same purpose but in FCC.

---
+++ ORV_coldchain_costs_app/monodoseOCC_vs_dose10FCC_mtd_calculations.R
@@ -0,0 +1,49 @@
+library('ggplot2')
+library('dplyr')
+
+source('./params.R', local = TRUE)
+source('./calculator_functions.R', local = TRUE)
+
+#params
+far_pop <- 250
+
+#monodose mobile team days
+monodose_OCC_far_trip_capacity <- calc_dose_capacity(vial_type = 'monodose' 
+                                                     , vax_vol = 21.09
+                                                     , equip_type = 'vaxCarr' #we assume a mobile team uses one vaccine carrier
+                                                     , with_ice = F)
+
+
+monodose_OCC_far_trip_capacity_expanded <- seq(1, 4.5, 0.5) * monodose_FCC_far_trip_capacity 
+
+team_days_mobile_monodose_noIce <- round(far_pop / monodose_OCC_far_trip_capacity_expanded, 1)
+
+
+#10 dose mobile team days
+wastage_dose10_mt_vect <- seq(1, 100/15, length.out = length(monodose_FCC_far_trip_capacity_expanded)) * dose10_wr_ft #wastage rates from the starting one for fixed teams 15% all the way to 100%
+
+
+dose10_FCC_far_trip_capacity <- calc_dose_capacity(vial_type = 'dose10' 
+                                                   , vax_vol = dose10_vial_vol[1]
+                                                   , equip_type = 'vaxCarr' #we assume a mobile team uses one vaccine carrier
+                                                   , with_ice = T)
+
+dose10_FCC_far_trip_eff_doses_vect <- dose10_FCC_far_trip_capacity * (1 - wastage_dose10_mt_vect)
+
+team_days_mobile_dose10_Ice <- round(far_pop / dose10_FCC_far_trip_eff_doses_vect, 1)  #there's a division by zero here, returning an infinity, so we'll replace it with NA to allow for better manipulation
+
+team_days_mobile_dose10_Ice[which(!is.finite(team_days_mobile_dose10_Ice))] <- NA 
+
+ratio_monodose_dose10_trip_capacity <- monodose_FCC_far_trip_capacity_expanded / dose10_FCC_far_trip_capacity
+
+
+team_days_comparison_df <- tibble(
+    dose10_wastage = rev(wastage_dose10_mt_vect),
+    trip_capacity_ratio = rev(ratio_monodose_dose10_trip_capacity),
+    team_days_dose10_far_FCC = rev(team_days_mobile_dose10_Ice),
+    team_days_monodose_far_OCC = rev(team_days_mobile_monodose_noIce)
+)
+
+df <- team_days_comparison_df %>% melt(id.vars = c('dose10_wastage', 'trip_capacity_ratio'), variable.name = 'strategy', value.name = 'team_days_mt')
+
+ggplot(data = df, aes(x = trip_capacity_ratio, y = team_days_mt, shape = strategy )) + geom_point() + geom_line()
