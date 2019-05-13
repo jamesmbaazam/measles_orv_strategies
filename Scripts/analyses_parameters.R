1621e7440a95c6a7964ff6ce297164ed08d76ba8
James Mba Azam <jamesazam@sun.ac.za>
2019-05-13 16:04:53 +0200

    created a parameters script for storing all the paramters needed for the SC and epi model functions

---
+++ Scripts/analyses_parameters.R
@@ -0,0 +1,10 @@
+#####################################
+#inputs
+######################################
+sc_model_param_set <- list(ambient_temp = c("below 40", "above 40")
+                , mf314_quant = 1
+                , buffer_stock = 0 #no buffer for now 
+                , near_pop = 1500 
+                , far_pop = 500
+                , site_team_alloc = 1 #number of teams allocated to site
+                )
