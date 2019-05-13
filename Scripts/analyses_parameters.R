f2f25d7297993044ba884e33ce97e86926ce8b99
James Mba Azam <jamesazam@sun.ac.za>
2019-05-13 17:35:03 +0200

    script for specifying the shared parameters between the SC and Epi models

--- Scripts/analyses_parameters.R
+++ Scripts/analyses_parameters.R
@@ -1,10 +1,24 @@
 #####################################
+#packages
+######################################
+library(dplyr)
+
+
+
+
+
+#####################################
 #inputs
 ######################################
-sc_model_param_set <- list(ambient_temp = c("below 40", "above 40")
+sc_model_params <- list(ambient_temp = c("below 40", "above 40")
                 , mf314_quant = 1
-                , buffer_stock = 0 #no buffer for now 
-                , near_pop = 1500 
-                , far_pop = 500
-                , site_team_alloc = 1 #number of teams allocated to site
+                , buffer_stock = 0 #value between 0 and 100
+                , monodose_vial_vol = 21.09
+                , dose10_vial_vol = c(2.1, 3)
                 )
+
+
+site_data <- tibble(near_pop = 1500 
+                           , far_pop = 500
+                           , site_team_alloc = 1 #number of teams allocated to site
+)
