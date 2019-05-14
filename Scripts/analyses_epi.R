929dd4577c5b228ae302069bca8f7be0bd844c67
James Mba Azam <jamesazam@sun.ac.za>
2019-05-14 17:10:38 +0200

    initial attempt at running the sc + orv results for 10-dose. Doesn't look okay.

--- Scripts/analyses_epi.R
+++ Scripts/analyses_epi.R
@@ -27,18 +27,23 @@ source('scripts/analyses_supply_chain.R')
 # beta <- R0 / IP
 # 
 # cam
-campaign_duration <- spread(td_results, strategy, team_days)
+
+delay_to_start <- dplyr::rename(freezing_time_results, delay_to_start = time)
+campaign_duration <- subset(td_results, team_type == 'Mobile team', select = -team_type)
+sc_to_epi_inputs <- left_join(campaign_duration, delay_to_start, by = 'strategy')
+
 
 xstrt <- c(S = 1 - 1 / N, E = 0, I = 1 / N, R = 0, K = 0)
 
 
 
-orv_dat <- p_red(R = R0, 
-                vaccine_efficacy = 0.95
-                 , target_vaccination = 0.7
-                 , intervention_length = 15
-                 , N = site_table$added_sites$far_pop
-                 )
+orv_dose10 <- p_red(R = orv_model_params$R0
+                    , vaccine_efficacy = orv_model_params$vaccine_efficacy
+                    , intervention_length = as.numeric(subset(sc_to_epi_inputs, strategy == '10-dose FCC')['team_days'])
+                    , N = site_data$far_pop
+                    , vax_rate = as.numeric(sc_model_params$vax_rate['fixed_team'])
+                    , mtime = orv_model_params$model_time
+                    )
 
 
 plot(orv_dat)
