74fb89a9c5ec166aeb6259062378aee1444c1d86
James Mba Azam <jamesazam@sun.ac.za>
2019-05-15 14:46:14 +0200

    added a brute force way of linking the sc outputs and inputs to the epi model to produce epidemic curves across the fours strategies

--- Scripts/analyses_epi.R
+++ Scripts/analyses_epi.R
@@ -17,33 +17,100 @@ source('scripts/analyses_supply_chain.R')
 
 # R0 <- 20 
 # vaccine_efficacy <- 0.9
-# target_vaccination <- 1
-# intervention_length <- 10
-# mtime = 120
-# LP = 7 #LP = Latent period
-# IP = 7 #IP = Infectious period
-# N <- site_table$added_sites$far_pop
-# step = 1
-# beta <- R0 / IP
-# 
-# cam
+#mtime = 60
 
+#parameters
+steps = 1:60
+beta <- R0 / IP
+immunity_delay <- 7 # seven days before people are immune
 delay_to_start <- dplyr::rename(freezing_time_results, delay_to_start = time)
 campaign_duration <- subset(td_results, team_type == 'Mobile team', select = -team_type)
 sc_to_epi_inputs <- left_join(campaign_duration, delay_to_start, by = 'strategy')
 
-
-xstrt <- c(S = 1 - 1 / N, E = 0, I = 1 / N, R = 0, K = 0)
-
-
-
-orv_dose10 <- p_red(R = orv_model_params$R0
-                    , vaccine_efficacy = orv_model_params$vaccine_efficacy
-                    , intervention_length = as.numeric(subset(sc_to_epi_inputs, strategy == '10-dose FCC')['team_days'])
-                    , N = site_data$far_pop
-                    , vax_rate = as.numeric(sc_model_params$vax_rate['fixed_team'])
-                    , mtime = orv_model_params$model_time
-                    )
+#xstrt <- c(S = 1 - 1 / N, E = 0, I = 1 / N, R = 0, K = 0)
+xstrt <- c(S = site_data$far_pop
+           , E = 0
+           , I = 1
+           , R = 0
+           , K = 0
+)
 
 
-plot(orv_dat)
+# orv_dose10 <- p_red(R = orv_model_params$R0
+#                     , vaccine_efficacy = orv_model_params$vaccine_efficacy
+#                     , delay = as.numeric(subset(delay_to_start, strategy == 'Monodose FCC')['delay_to_start'])
+#                     , intervention_length = as.numeric(subset(sc_to_epi_inputs, strategy == 'Monodose FCC')['team_days'])
+#                     , N = site_data$far_pop
+#                     , vax_rate = as.numeric(sc_model_params$vax_rate['fixed_team'])
+#                     , mtime = orv_model_params$model_time
+#                     )
+# 
+# 
+# plot(orv_dat)
+
+#10-dose FCC parameters
+par_dose10FCC <- c(B = beta 
+                   , r = 1 / orv_model_params$LP
+                   , g = 1 / orv_model_params$IP
+                   , vax_eff = vaccine_efficacy
+                   , immunity_delay = orv_model_params$immune_response_timing
+                   , orv_dur = as.numeric(subset(sc_to_epi_inputs, strategy == '10-dose FCC')['team_days'])
+                   , campaign_delay = as.numeric(subset(delay_to_start, strategy == '10-dose FCC')['delay_to_start'])
+                   , vax_rate = as.numeric(sc_model_params$vax_rate['fixed_team'])
+)
+
+#monodose FCC parameters
+par_monodoseFCC <- c(B = beta 
+                     , r = 1 / orv_model_params$LP
+                     , g = 1 / orv_model_params$IP
+                     , vax_eff = vaccine_efficacy
+                     , immunity_delay = orv_model_params$immune_response_timing
+                     , orv_dur = as.numeric(subset(sc_to_epi_inputs, strategy == 'Monodose FCC')['team_days'])
+                     , campaign_delay = as.numeric(subset(delay_to_start, strategy == 'Monodose FCC')['delay_to_start'])
+                     , vax_rate = as.numeric(sc_model_params$vax_rate['fixed_team'])
+)
+
+
+#Mixed FCC parameters
+par_mixedFCC <- c(B = beta 
+                  , r = 1 / orv_model_params$LP
+                  , g = 1 / orv_model_params$IP
+                  , vax_eff = vaccine_efficacy
+                  , immunity_delay = orv_model_params$immune_response_timing
+                  , orv_dur = as.numeric(subset(sc_to_epi_inputs, strategy == 'Mixed FCC')['team_days'])
+                  , campaign_delay = as.numeric(subset(delay_to_start, strategy == 'Mixed FCC')['delay_to_start'])
+                  , vax_rate = as.numeric(sc_model_params$vax_rate['fixed_team'])
+)
+
+#Partial OCC parameters
+par_partOCC <- c(B = beta 
+                 , r = 1 / orv_model_params$LP
+                 , g = 1 / orv_model_params$IP
+                 , vax_eff = vaccine_efficacy
+                 , immunity_delay = orv_model_params$immune_response_timing
+                 , orv_dur = as.numeric(subset(sc_to_epi_inputs, strategy == 'Part OCC')['team_days'])
+                 , campaign_delay = as.numeric(subset(delay_to_start, strategy == 'Part OCC')['delay_to_start'])
+                 , vax_rate = as.numeric(sc_model_params$vax_rate['fixed_team'])
+)
+
+
+
+
+#model runs
+out_dose10FCC <- as.data.frame(lsoda(xstrt, steps, simod, par_dose10FCC))
+out_monodoseFCC <- as.data.frame(lsoda(xstrt, steps, simod, par_monodoseFCC))
+out_mixedFCC <- as.data.frame(lsoda(xstrt, steps, simod, par_mixedFCC))
+out_partOCC <- as.data.frame(lsoda(xstrt, steps, simod, par_partOCC))
+
+
+#plots
+
+incidence_plot <- ggplot(data = out_dose10FCC) + geom_point(aes(x = time, y = K)) + geom_line(aes(x = time, y = K)) +
+    geom_point(data = out_monodoseFCC, aes(x = time, y = K), color = 'red') + geom_line(data = out_monodoseFCC, aes(x = time, y = K), color = 'red') +
+    geom_point(data = out_mixedFCC, aes(x = time, y = K), color = 'blue') + geom_line(data = out_mixedFCC, aes(x = time, y = K), color = 'blue') +
+    geom_point(data = out_partOCC, aes(x = time, y = K), color = 'green') + geom_line(data = out_partOCC, aes(x = time, y = K), color = 'green') +
+    labs(x = 'time (days)', y = 'Incidence')
+
+plot(incidence_plot)
+
+
