35343bd33693c42340ab4ba52e3de1d27681aee1
James Mba Azam <jamesazam@sun.ac.za>
2019-07-05 14:37:53 +0200

    made major changes to improve code navigation by introduction code sections. Nothing about the code itself has changed.

--- Scripts/wrappers_supply_chain.R
+++ Scripts/wrappers_supply_chain.R
@@ -5,13 +5,14 @@ source('./scripts/parameters.R')
 source('./scripts/supply_chain_functions.R')
 
 
+#NB: Create code sections by following a comment with 4 or more dashes (----), 
+# subsections by following a comment with 4 or more equal signs (====),
+# and subsubsections following a comment with 4 or more hashes (####)
 
 
-############################################################################
-#'analyse_prep_delay(): wrapper to combine all the supply chain functions and analyse 
+# analyse_prep_delay() ---- 
+#' wrapper to combine all the supply chain functions and analyse 
 #'the logistical needs and time to commence a campaign for each strategy
-############################################################################
-
 analyse_prep_delay <- function(strategy_name
                                , fixed_team_with_dose10 # options = if "T", 10 dose, else monodose
                                , fixed_team_with_ice # options = if "T", ice is used, else, no ice
@@ -29,11 +30,10 @@ analyse_prep_delay <- function(strategy_name
                                , monodose_vial_volume = sc_model_params$monodose_vial_vol
 ){
     
-    #################
-    #fixed team calculations
-    ################
+   
+    ##Doses and equipment calculations ====
     
-    #doses needed for near population
+    ### Fixed post - number of doses ####
     doses_fixed_team <-  calc_doses_required(df = site_details
                                              , site_rows_selected = site_row
                                              , is_dose10 = fixed_team_with_dose10
@@ -42,16 +42,7 @@ analyse_prep_delay <- function(strategy_name
                                              , buffer_size = sc_model_params$buffer_stock
                                              )
     
-    doses_mobile_team <-  calc_doses_required(df = site_details
-                                              , site_rows_selected = site_row
-                                              , is_dose10 = mobile_team_with_dose10
-                                              , pop_type = 'far'
-                                              , ovwastage = ifelse(mobile_team_with_dose10, sc_model_params$dose10_ovw_mobile_team, sc_model_params$monodose_ovw_mobile_team)
-                                              , buffer_size = sc_model_params$buffer_stock
-                                              )
-    
-    #determine passive cold chain needs
-    ######################################
+    ### Fixed post - passive cold chain ####
     #Here, I am assuming that vaccine carriers are transported with ice but without vaccines to the site.
     #'The RCW25s are used to transport the vaccines to the site and then transferred into the vax carrier for 
     #'for administration.
@@ -73,12 +64,17 @@ analyse_prep_delay <- function(strategy_name
                                                                   )
     
     
-    #################
-    #Mobile team calculations
-    ################
+    ### Mobile teams - number of doses ####
+    doses_mobile_team <-  calc_doses_required(df = site_details
+                                              , site_rows_selected = site_row
+                                              , is_dose10 = mobile_team_with_dose10
+                                              , pop_type = 'far'
+                                              , ovwastage = ifelse(mobile_team_with_dose10, sc_model_params$dose10_ovw_mobile_team, sc_model_params$monodose_ovw_mobile_team)
+                                              , buffer_size = sc_model_params$buffer_stock
+    )
     
-    #determine passive cold chain needs
-    ######################################
+    ###Mobile teams - passive cold chain needs ####
+   
     #Here, I am assuming that mobile teams only need a vaccine carrier. 
     #' vaccine carriers are transported with ice and vaccines to the site.
     RCW25_required_mobile_team <- calc_transport_equipment_needs(equip_type = 'rcw25'
@@ -97,16 +93,17 @@ analyse_prep_delay <- function(strategy_name
     
     
     
-    ##############################################
-    #Ice pack needs # total number of 0.6L ice packs = number of RCW25 needed * number of ice packs needed per RCW25
-    ###############################################
-    
+  
+    ## Ice pack needs calculations ====
+    # total number of 0.6L ice packs = number of RCW25 needed * number of ice packs needed per RCW25
+   
+    ### RCW25 and vaccince carrier needs ####
     #ice packs needed for each equipment type, based on the ambient temperature
     RCW25_icepack_needs <-  compute_rcw25_icepacks(sc_model_params$ambient_temp[1])
     
     vaxCarr_icepack_needs <- compute_vaxCarr_icepacks(sc_model_params$ambient_temp[1])
     
-    #Fixed post
+    #### Fixed post - Ice packs required ####
     RCW25_icepack_needs_fixed_team <- calc_icepack_tot_quant(equipment_quantity = ifelse(fixed_team_with_ice, RCW25_required_fixed_team, 0)
                                                              , icepacks_per_equipment = RCW25_icepack_needs
     ) # total number of 0.6L ice packs = number of RCW25 needed * number of ice packs needed per RCW25
@@ -114,7 +111,7 @@ analyse_prep_delay <- function(strategy_name
                                                                , icepacks_per_equipment = vaxCarr_icepack_needs
     ) 
     
-    #mobile teams
+    ### Mobile team - Ice packs required ####
     RCW25_icepack_needs_mobile_team <- calc_icepack_tot_quant(equipment_quantity = ifelse(mobile_team_with_ice, RCW25_required_mobile_team, 0)
                                                               , icepacks_per_equipment = RCW25_icepack_needs
     )
@@ -131,33 +128,35 @@ analyse_prep_delay <- function(strategy_name
     #dose10_FCC_init_icepack_quant <- dose10_FCC_RCW25_icepack_needs_total + dose10_FCC_vaxCarr_icepack_needs_total
     
     
-    ############################################
-    # freezing time: # Time it takes to freeze depends on how many freezers are available and their capacity. I currently assume that we only use the MF314 freezer, which is the largest, and I specify the quantity at the beginning of this script
-    ##############################################
     
-    #fixed post team
+    ## Freezing time calculations ====
+    # Time it takes to freeze depends on how many freezers are available and their capacity. I currently assume that we only use the MF314 freezer, which is the largest, and I specify the quantity at the beginning of this script
+    
+    
+    ### Fixed post team  - freezing time ####
     freezing_time_fixed_team <- calc_freezing_time(mf314_available = mf314
                                                    , large_icepacks_quantity = ifelse(fixed_team_with_ice, RCW25_icepack_needs_fixed_team, 0) 
                                                    , small_icepacks_quantity = ifelse(fixed_team_with_ice, vaxCarr_icepack_needs_fixed_team, 0)
     )
     
+    
+    ### Mobile team  - freezing time ####
     freezing_time_mobile_team <- calc_freezing_time(mf314_available = mf314
                                                     , large_icepacks_quantity = ifelse(mobile_team_with_ice, RCW25_icepack_needs_mobile_team, 0)
                                                     , small_icepacks_quantity = ifelse(mobile_team_with_ice, vaxCarr_icepack_needs_mobile_team, 0)
     )
     
     
-    
-    
-    ######################################################
-    #When will the campaign start?
-    ######################################################
-    #TODO I will modify the function so that it returns which team heads out first if the routing is 'asap"
+    ## Campaign delay calculation ====
+    #'this function uses the results of mobile and fixed post team freezing times and more importantly, the routing 
+    #' decision to determine how long a strategy will be delayed before a campaign can start 
     campaign_delay <- calc_campaign_start(fixedT_freeze_time = freezing_time_fixed_team
                                           , mobileT_freeze_time = freezing_time_mobile_team
                                           , team_routing = team_dispatch
     )
     
+    
+    ## Results - Campaign delays ====
     out <- data.frame(strategy = strategy_name,
                       ft_vial_type = ifelse(fixed_team_with_dose10, 'dose10', 'monodose')
                       , ft_doses_required = doses_fixed_team
@@ -183,13 +182,11 @@ analyse_prep_delay <- function(strategy_name
 
 
 
-################################################################################
-#' ##analyse_team_days(): a wrapper to determine what the team days is for a strategy.
+
+# analyse_team_days() ----
+#' a wrapper to determine what the team days is for a strategy.
 #' Given tau team days, we need a single team to spend tau days, or two teams to spend 
 #' tau/2 days, and for n teams, tau/n. It is to serve as the campaign duration on a site 
-#' ################################################################################
-
-#size of near population
 
 analyse_team_days <- function(strategy_name
                               , fixed_team_with_dose10 # options = if "T", 10 dose, else monodose
@@ -208,6 +205,14 @@ analyse_team_days <- function(strategy_name
 ){
     
     if (browse)browse 
+    
+    ##Fixed post team days ====
+    #team days needed by fixed teams, NOT CONSTRAINED by volume/space - we assume they can transport all they need per trip
+    team_days_fixed_team <- round(site_details$near_pop / tp_fixed, 1) #computationally, we see the number doses as the number of expected people. The "final number of doses" here have already accounted for the buffer
+    
+    
+    
+    ##Mobile teams team days ====
     #Mobile teams only use a vaccine carrier, hence, are contrained by how much they can transport
     mobile_team_vol_capacity <- calc_dose_capacity(vial_type = ifelse(mobile_team_with_dose10, 'dose10', 'monodose')
                                                    , vax_vol = ifelse(mobile_team_with_dose10, dose10_vial_volume, monodose_vial_volume)
@@ -216,9 +221,6 @@ analyse_team_days <- function(strategy_name
     )
     
 
-    
-    #team days needed by fixed teams, NOT CONSTRAINED by volume/space - we assume they can transport all they need per trip
-    team_days_fixed_team <- round(site_details$near_pop / tp_fixed, 1) #computationally, we see the number doses as the number of expected people. The "final number of doses" here have already accounted for the buffer
     
     #team days needed by mobile teams, CONSTRAINED by volume/space - we assume per trip, they can only transport as much as the vaccine carrier allows; furthermore, for the 10-dose, due to open vial wastage, they end up wasting a percentage of the doses, hence they have to make more trips to account for that
     #10-dose mobile campaign team days are affected by the effective doses, which is the the total capacity they can carry less of how many are expected to be wasted.
@@ -235,6 +237,7 @@ analyse_team_days <- function(strategy_name
         )
     }
     
+    ## Results - Team days  ====
     out <- data.frame(strategy = strategy_name
                       , ft_vial_type = ifelse(fixed_team_with_dose10, 'dose10', 'monodose')
                       , ft_with_ice = ifelse(fixed_team_with_ice, 'yes', 'no')
