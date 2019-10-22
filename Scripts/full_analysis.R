7d343751bb9281a0bc20adf75689e6896446453e
James Mba Azam <jamesazam@sun.ac.za>
2019-10-22 15:01:45 +0200

    included a monodose OCC strategy

--- Scripts/full_analysis.R
+++ Scripts/full_analysis.R
@@ -60,6 +60,22 @@ strategy_analysis_list <- list(
                                        , mobile_team_with_ice = T
                                        , team_dispatch = 'parallel'
     ),
+    # monodose occ
+    monodose_occ_asap = data.frame(strategy_name = 'monodose_occ_asap'
+                                   , fixed_team_with_dose10 = F
+                                   , fixed_team_with_ice = F
+                                   , mobile_team_with_dose10 = F
+                                   , mobile_team_with_ice = F
+                                   , team_dispatch = 'asap'
+    ),
+    
+    monodose_occ_parallel = data.frame(strategy_name = 'monodose_occ_parallel'
+                                       , fixed_team_with_dose10 = F
+                                       , fixed_team_with_ice = F
+                                       , mobile_team_with_dose10 = F
+                                       , mobile_team_with_ice = F
+                                       , team_dispatch = 'parallel'
+    ),
     #mixed fcc
     mixed_fcc_asap = data.frame(strategy_name = 'part_occ_asap'
                                 , fixed_team_with_dose10 = T
