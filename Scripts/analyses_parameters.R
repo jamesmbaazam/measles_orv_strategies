e9ad096cea3688fd4b3137f475bbc6c14a0e333a
James Mba Azam <jamesazam@sun.ac.za>
2019-10-23 17:49:00 +0200

    this is the population size threshold below which I think the monodose OCC is expected to be the best strategy

--- Scripts/analyses_parameters.R
+++ Scripts/analyses_parameters.R
@@ -34,7 +34,7 @@ orv_model_params <- list(near_pop_R0 = 12
 
 
 site_data <- tibble(near_pop = 1000 
-                           , far_pop = 500
+                           , far_pop = 169
                            , site_team_alloc = 1 #number of teams allocated to site
 )
 
