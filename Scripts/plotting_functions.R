a82b5f748821513dbb6e81c9c593fce5e40e1552
James Mba Azam <jamesazam@sun.ac.za>
2019-11-28 14:57:02 +0200

    added a function to create gap axis in ggplot2

--- Scripts/plotting_functions.R
+++ Scripts/plotting_functions.R
@@ -64,3 +64,39 @@ shiny_plot_theme <-  theme(title = element_text(size = 12,
                                                 face = 'bold'
 )
 )
+
+
+#function for creating gap axes (solution from SO)
+library(scales)
+squish_trans <- function(from, to, factor) {
+    
+    trans <- function(x) {
+        
+        # get indices for the relevant regions
+        isq <- x > from & x < to
+        ito <- x >= to
+        
+        # apply transformation
+        x[isq] <- from + (x[isq] - from)/factor
+        x[ito] <- from + (to - from)/factor + (x[ito] - to)
+        
+        return(x)
+    }
+    
+    inv <- function(x) {
+        
+        # get indices for the relevant regions
+        isq <- x > from & x < from + (to - from)/factor
+        ito <- x >= from + (to - from)/factor
+        
+        # apply transformation
+        x[isq] <- from + (x[isq] - from) * factor
+        x[ito] <- to + (x[ito] - (from + (to - from)/factor))
+        
+        return(x)
+    }
+    
+    # return the transformation
+    return(trans_new("squished", trans, inv))
+}
+
