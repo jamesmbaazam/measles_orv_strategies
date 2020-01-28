9d62c62f33cb4d3a85ad87a660e2424f91401ae5
James Mba Azam <jamesazam@sun.ac.za>
2020-01-28 14:17:52 +0200

    automated the generation of strategy names

--- Scripts/strategy_list_complete.R
+++ Scripts/strategy_list_complete.R
@@ -1,90 +1,22 @@
-#all possible combinations
-strategy_scenarios_complete <- expand.grid(fixed_team_with_dose10 = c(T, F)
-                        , fixed_team_with_ice = c(T, F)
-                        , mobile_team_with_dose10 = c(T, F)
-                        , mobile_team_with_ice = c(T, F)
-                        , team_dispatch = c('parallel', 'asap')
-                        )
+library(dplyr)
 
-#Full list of strategies
-strategy_analysis_list <- list(
-    # 10-dose fcc
-    dose10_fcc_asap = data.frame(strategy_name = 'dose10_fcc_asap',
-                                 fixed_team_with_dose10 = T
-                                 , fixed_team_with_ice = T
-                                 , mobile_team_with_dose10 = T
-                                 , mobile_team_with_ice = T
-                                 , team_dispatch = 'asap'
-    ),
-    dose10_fcc_parallel = data.frame(strategy_name = 'dose10_fcc_parallel'
-                                     , fixed_team_with_dose10 = T
-                                     , fixed_team_with_ice = T
-                                     , mobile_team_with_dose10 = T
-                                     , mobile_team_with_ice = T
-                                     , team_dispatch = 'parallel'
-    ),
-    # monodose fcc
-    monodose_fcc_asap = data.frame(strategy_name = 'monodose_fcc_asap'
-                                   , fixed_team_with_dose10 = F
-                                   , fixed_team_with_ice = T
-                                   , mobile_team_with_dose10 = F
-                                   , mobile_team_with_ice = T
-                                   , team_dispatch = 'asap'
-    ),
-    monodose_fcc_parallel = data.frame(strategy_name = 'monodose_fcc_parallel'
-                                       , fixed_team_with_dose10 = F
-                                       , fixed_team_with_ice = T
-                                       , mobile_team_with_dose10 = F
-                                       , mobile_team_with_ice = T
-                                       , team_dispatch = 'parallel'
-    ),
-    # monodose occ
-    monodose_occ_asap = data.frame(strategy_name = 'monodose_occ_asap'
-                                   , fixed_team_with_dose10 = F
-                                   , fixed_team_with_ice = F
-                                   , mobile_team_with_dose10 = F
-                                   , mobile_team_with_ice = F
-                                   , team_dispatch = 'asap'
-    ),
-    
-    monodose_occ_parallel = data.frame(strategy_name = 'monodose_occ_parallel'
-                                       , fixed_team_with_dose10 = F
-                                       , fixed_team_with_ice = F
-                                       , mobile_team_with_dose10 = F
-                                       , mobile_team_with_ice = F
-                                       , team_dispatch = 'parallel'
-    ),
-    #mixed fcc
-    mixed_fcc_asap = data.frame(strategy_name = 'part_occ_asap'
-                                , fixed_team_with_dose10 = T
-                                , fixed_team_with_ice = T
-                                , mobile_team_with_dose10 = F
-                                , mobile_team_with_ice = T
-                                , team_dispatch = 'asap'
-    ),
-    
-    mixed_fcc_parallel = data.frame(strategy_name = 'part_occ_parallel'
-                                    , fixed_team_with_dose10 = T
-                                    , fixed_team_with_ice = T
-                                    , mobile_team_with_dose10 = F
-                                    , mobile_team_with_ice = T
-                                    , team_dispatch = 'parallel'
-    ),
-    
-    #part occ
-    part_occ_asap = data.frame(strategy_name = 'part_occ_asap'
-                               , fixed_team_with_dose10 = T
-                               , fixed_team_with_ice = T
-                               , mobile_team_with_dose10 = F
-                               , mobile_team_with_ice = F
-                               , team_dispatch = 'asap'
-    ),
-    
-    part_occ_parallel = data.frame(strategy_name = 'part_occ_parallel'
-                                   , fixed_team_with_dose10 = T
-                                   , fixed_team_with_ice = T
-                                   , mobile_team_with_dose10 = F
-                                   , mobile_team_with_ice = F
-                                   , team_dispatch = 'parallel'
-    )
+# all possible combinations
+strategy_list <- expand.grid(
+  ft_with_dose10 = c(T, F),
+  ft_with_ice = c(T, F),
+  mt_with_dose10 = c(T, F),
+  mt_with_ice = c(T, F),
+  dispatch = c("parallel", "asap")
 )
+
+
+#dynamically create the strategy names from their composition
+strategy_list <- strategy_list %>%
+  mutate(strategy = sprintf(
+    "%s_%s_%s",
+    c("monodose", "mixed", "dose10")[1 + ft_with_dose10 + mt_with_dose10],
+    c("occ", "pcc", "fcc")[1 + ft_with_ice + mt_with_ice],
+    dispatch
+  ))
+
+
