487d1029e58d1a63e1222b5a4821362d02ac5f77
James Mba Azam <jamesazam@sun.ac.za>
2020-02-27 14:55:42 +0200

    added an exmaple script to run the constrained analysis

---
+++ Scripts/epidemics7_analysis/example_constrained_analysis.R
@@ -0,0 +1,50 @@
+dose10_occ_parallel_prep_delay <- analyse_prep_delay(
+  strategy_name = "dose10_occ_parallel",
+  fixed_team_with_dose10 = T,
+  fixed_team_with_ice = F,
+  mobile_team_with_dose10 = T,
+  mobile_team_with_ice = F,
+  team_dispatch = "parallel",
+  site_details = data.frame(
+    location_id = 1,
+    near_pop = 10000,
+    far_pop = 20000
+  ),
+  fixed_team_equip_type = "both",
+  mobile_team_equip_type = "vaxCarr",
+  n_teams_fixed = 1,
+  n_teams_mobile = 1,
+  mf314 = 1,
+  rcw25_ice_replacement_days = 2,
+  ambient_temperature = sc_model_params$ambient_temp[1],
+  dose10_vial_volume = sc_model_params$dose10_vial_vol[1],
+  monodose_vial_volume = sc_model_params$monodose_vial_vol,
+  res_type = "simple"
+)
+
+
+dose10_occ_campaign_metrics <- estim_team_metrics(
+  strategy_name = "dose10_occ_parallel",
+  fixed_team_with_dose10 = T,
+  fixed_team_with_ice = F,
+  mobile_team_with_dose10 = T,
+  mobile_team_with_ice = F,
+  site_details = data.frame(
+    location_id = 1,
+    near_pop = 10000,
+    far_pop = 20000
+  ),
+  mobile_team_equip_type = "vaxCarr",
+  n_teams_fixed = 1,
+  n_teams_mobile = 1,
+  dose10_vial_volume = sc_model_params$dose10_vial_vol[1],
+  monodose_vial_volume = sc_model_params$monodose_vial_vol,
+  site_campaign_dur = 10,
+  site_campaign_mt_delay = dose10_occ_parallel_prep_delay$mt_freezing_time,
+  site_campaign_ft_delay = dose10_occ_parallel_prep_delay$ft_freezing_time,
+  ft_team_performance = 450,
+  mt_team_performance = 250,
+  browse = F
+)
+
+dose10_occ_campaign_metrics
