1614c35f5d2e7103c062f0e0bab9d537ec7e1eda
James Mba Azam <jamesazam@sun.ac.za>
2020-03-04 15:53:08 +0200

    commented out the example analysis for dose10 occ and ran the analysis for a full range of parameters.

--- Scripts/epidemics7_analysis/example_constrained_analysis.R
+++ Scripts/epidemics7_analysis/example_constrained_analysis.R
@@ -1,30 +1,31 @@
-source('./scripts/wrappers_supply_chain.R')
+source("./scripts/wrappers_supply_chain.R")
+source("./scripts/epidemics7_analysis/simulation_params.R")
 
-teams <- data.frame(ft = 30, mt = 30)
+#teams <- data.frame(ft = 30, mt = 30)
 
-dose10_occ_campaign_metrics <- estim_campaign_metrics(
-  strategy_name = "dose10_occ_parallel",
-  fixed_team_with_dose10 = T,
-  fixed_team_with_ice = F,
-  mobile_team_with_dose10 = T,
-  mobile_team_with_ice = F,
-  site_details = data.frame(
-    location_id = 1,
-    near_pop = 100000,
-    far_pop = 50000
-  ),
-  mobile_team_equip_type = "vaxCarr",
-  n_teams_fixed = teams$ft,
-  n_teams_mobile = teams$mt,
-  dose10_vial_volume = sc_model_params$dose10_vial_vol[1],
-  monodose_vial_volume = sc_model_params$monodose_vial_vol,
-  site_campaign_dur_constraint = 10,
-  ft_team_performance = 450,
-  mt_team_performance = 250,
-  browse = F
-)
-
-print(dose10_occ_campaign_metrics)
+# dose10_occ_campaign_metrics <- estim_campaign_metrics(
+#   strategy_name = "dose10_occ_parallel",
+#   fixed_team_with_dose10 = T,
+#   fixed_team_with_ice = F,
+#   mobile_team_with_dose10 = T,
+#   mobile_team_with_ice = F,
+#   site_details = data.frame(
+#     location_id = 1,
+#     near_pop = 100000,
+#     far_pop = 50000
+#   ),
+#   mobile_team_equip_type = "vaxCarr",
+#   n_teams_fixed = teams$ft,
+#   n_teams_mobile = teams$mt,
+#   dose10_vial_volume = sc_model_params$dose10_vial_vol[1],
+#   monodose_vial_volume = sc_model_params$monodose_vial_vol,
+#   site_campaign_dur_constraint = 10,
+#   ft_team_performance = 450,
+#   mt_team_performance = 250,
+#   browse = F
+# )
+# 
+# print(dose10_occ_campaign_metrics)
 
 
 
@@ -83,7 +84,9 @@ campaign_metrics <- sim_params_table %>%
   })
 
 
-#visualisations
-campaign_metrics %>% ggplot(aes(near_pop, fixed_teams)) + 
-  geom_raster(aes(fill = site_campaign_dur_gain)) + 
-  facet_wrap(strategy ~ mt_equip_type)
+# visualisations
+time_gained_plot <- campaign_metrics %>% ggplot(aes(near_pop, far_pop)) +
+  geom_raster(aes(fill = site_campaign_dur_gain)) +
+  facet_wrap(~ strategy + mt_equip_type)
+
+plot(time_gained_plot)
