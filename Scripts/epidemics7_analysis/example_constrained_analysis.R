4f42ffec9554da7049bbf6aaee295af187c2c16d
James Mba Azam <jamesazam@sun.ac.za>
2020-03-04 17:37:37 +0200

    commented out the example code

--- Scripts/epidemics7_analysis/example_constrained_analysis.R
+++ Scripts/epidemics7_analysis/example_constrained_analysis.R
@@ -30,27 +30,27 @@ source("./scripts/epidemics7_analysis/simulation_params.R")
 
 
 
-estim_campaign_metrics(
-  strategy_name = "dose10_occ_parallel",
-  fixed_team_with_dose10 = T,
-  fixed_team_with_ice = F,
-  mobile_team_with_dose10 = T,
-  mobile_team_with_ice = F,
-  site_details = data.frame(
-    location_id = 1,
-    near_pop = 100000,
-    far_pop = 50000
-  ),
-  mobile_team_equip_type = "vaxCarr",
-  n_teams_fixed = teams$ft,
-  n_teams_mobile = teams$mt,
-  dose10_vial_volume = sc_model_params$dose10_vial_vol[1],
-  monodose_vial_volume = sc_model_params$monodose_vial_vol,
-  site_campaign_dur_constraint = 10,
-  ft_team_performance = 450,
-  mt_team_performance = 250,
-  browse = F
-)
+# estim_campaign_metrics(
+#   strategy_name = "dose10_occ_parallel",
+#   fixed_team_with_dose10 = T,
+#   fixed_team_with_ice = F,
+#   mobile_team_with_dose10 = T,
+#   mobile_team_with_ice = F,
+#   site_details = data.frame(
+#     location_id = 1,
+#     near_pop = 100000,
+#     far_pop = 50000
+#   ),
+#   mobile_team_equip_type = "vaxCarr",
+#   n_teams_fixed = teams$ft,
+#   n_teams_mobile = teams$mt,
+#   dose10_vial_volume = sc_model_params$dose10_vial_vol[1],
+#   monodose_vial_volume = sc_model_params$monodose_vial_vol,
+#   site_campaign_dur_constraint = 10,
+#   ft_team_performance = 450,
+#   mt_team_performance = 250,
+#   browse = F
+# )
 
 
 
