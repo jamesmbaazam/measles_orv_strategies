305579adf98c4508b28dfbf3e9a927ad5abc951f
James Mba Azam <jamesazam@sun.ac.za>
2020-03-11 17:31:57 +0200

    removed some commented code

--- scripts/epidemics7_analysis/example_constrained_analysis.R
+++ scripts/epidemics7_analysis/example_constrained_analysis.R
@@ -1,58 +1,51 @@
 source("./scripts/wrappers_supply_chain.R")
 source("./scripts/epidemics7_analysis/simulation_params.R")
 
-#teams <- data.frame(ft = 30, mt = 30)
 
-# dose10_occ_campaign_metrics <- estim_campaign_metrics(
-#   strategy_name = "dose10_occ_parallel",
-#   fixed_team_with_dose10 = T,
-#   fixed_team_with_ice = F,
-#   mobile_team_with_dose10 = T,
-#   mobile_team_with_ice = F,
-#   site_details = data.frame(
-#     location_id = 1,
-#     near_pop = 100000,
-#     far_pop = 50000
-#   ),
-#   mobile_team_equip_type = "vaxCarr",
-#   n_teams_fixed = teams$ft,
-#   n_teams_mobile = teams$mt,
-#   dose10_vial_volume = sc_model_params$dose10_vial_vol[1],
-#   monodose_vial_volume = sc_model_params$monodose_vial_vol,
-#   site_campaign_dur_constraint = 10,
-#   ft_team_performance = 450,
-#   mt_team_performance = 250,
-#   browse = F
-# )
-# 
-# print(dose10_occ_campaign_metrics)
-
-
-
-
-# estim_campaign_metrics(
-#   strategy_name = "dose10_occ_parallel",
-#   fixed_team_with_dose10 = T,
-#   fixed_team_with_ice = F,
-#   mobile_team_with_dose10 = T,
-#   mobile_team_with_ice = F,
-#   site_details = data.frame(
-#     location_id = 1,
-#     near_pop = 100000,
-#     far_pop = 50000
-#   ),
-#   mobile_team_equip_type = "vaxCarr",
-#   n_teams_fixed = teams$ft,
-#   n_teams_mobile = teams$mt,
-#   dose10_vial_volume = sc_model_params$dose10_vial_vol[1],
-#   monodose_vial_volume = sc_model_params$monodose_vial_vol,
-#   site_campaign_dur_constraint = 10,
-#   ft_team_performance = 450,
-#   mt_team_performance = 250,
-#   browse = F
-# )
+#calculate delays prior to campaign start
+campaign_delay_results <- sim_params_table %>%
+  rowwise() %>%
+  do({
+    with(
+      .,
+      analyse_prep_delay(
+        strategy_name = strategy,
+        fixed_team_with_dose10 = ft_with_dose10,
+        fixed_team_with_ice = ft_with_ice,
+        mobile_team_with_dose10 = mt_with_dose10,
+        mobile_team_with_ice = mt_with_ice,
+        team_dispatch = dispatch,
+        site_details = data.frame(location_id = location_id, 
+                                  near_pop = near_pop, 
+                                  far_pop = far_pop
+                                  ),
+        fixed_team_equip_type = "both",
+        mobile_team_equip_type = equip_type,
+        n_teams_fixed = 1,
+        n_teams_mobile = 1,
+        rcw25_ice_replacement_days = 2,
+        mf314 = 1, 
+        ambient_temperature = sc_model_params$ambient_temp[1], 
+        dose10_vial_volume = sc_model_params$dose10_vial_vol[1], 
+        monodose_vial_volume = sc_model_params$monodose_vial_vol[1], 
+        res_type = 'detailed' 
+      )
+    )
+  })
 
 
+## Remove some columns ==== 
+campaign_delay_results_actual <- campaign_delay_results %>% 
+  select(-c(near_pop, far_pop, ft_vial_type, ft_equip_type, 
+            mt_vial_type, ft_doses_required, mt_doses_required, 
+            ft_RCW25, mt_RCW25, ft_vaxCarr, 
+            mt_vaxCarr, ft_icepacks_large, mt_icepacks_large, 
+            ft_icepacks_small, mt_icepacks_small, team_leaving_first
+            )
+         )
+
+
+View(campaign_delay_results_actual)
 
 campaign_metrics <- sim_params_table %>%
   rowwise() %>%
@@ -83,10 +76,12 @@ campaign_metrics <- sim_params_table %>%
     )
   })
 
+View(campaign_metrics)
+#library(xlsx)
+#write.xlsx(x = campaign_metrics, file = './model_output/campaign_metrics.xlsx')
+
+sc_analysis_full <- left_join(campaign_delay_results_actual, 
+                         campaign_metrics)
+
 
 # visualisations
-time_gained_plot <- campaign_metrics %>% ggplot(aes(near_pop, far_pop)) +
-  geom_raster(aes(fill = site_campaign_dur_gain)) +
-  facet_wrap(~ strategy + mt_equip_type)
-
-plot(time_gained_plot)
