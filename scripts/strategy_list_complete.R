a930b0ccee304335b4142fced3f668d356c52483
James Mba Azam <jamesazam@sun.ac.za>
2020-03-24 22:13:43 +0200

    reformulated the complete strategy list
    The old method of using expand grid was confusing and potentially erroneous, producing redundant strategies

--- scripts/strategy_list_complete.R
+++ scripts/strategy_list_complete.R
@@ -1,12 +1,12 @@
 library(dplyr)
 
 # all possible combinations
-strategies <- expand.grid(
-  ft_with_dose10 = c(T, F),
-  ft_with_ice = c(T, F),
-  mt_with_dose10 = c(T, F),
-  mt_with_ice = c(T, F),
-  dispatch = c("parallel", "asap")
+strategies <- tibble(
+  ft_with_dose10 = rep(c(T, T, T, T, F, F), times = 2),
+  ft_with_ice = rep(c(T, F, T, T, T, F), times = 2),
+  mt_with_dose10 = rep(c(T, T, F, F, F, F), times = 2),
+  mt_with_ice = rep(c(T, F, T, F, T, F), times = 2),
+  dispatch = rep(c("parallel", "asap"), each = length(ft_with_dose10)/2)
 ) %>%
   #dynamically create the strategy names from their composition
   mutate(strategy = as.factor(sprintf(
