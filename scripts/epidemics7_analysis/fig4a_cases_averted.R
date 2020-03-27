6cb07f671f9940d40f0eb99aead1f0012c98e741
James Mba Azam <jamesazam@sun.ac.za>
2020-03-27 16:30:26 +0200

    script to generate the cases averted plot

---
+++ scripts/epidemics7_analysis/fig4a_cases_averted.R
@@ -0,0 +1,25 @@
+library(ggplot2)
+library(ggthemes)
+
+#laod plotting data
+cases_averted_df <- readRDS('./model_output/sc_epi_analysis_summary_10_teams.rds')
+
+#' visualisations
+fig4a_cases_averted <- ggplot(data = cases_averted_df) + 
+    geom_jitter(aes(x = campaign_duration, 
+                    y = average_coverage, 
+                    color = mt_equip_type,
+                    size = cases_averted), 
+                width = 0.10, 
+                height = 0.025
+    ) +
+    labs(title = 'Campaign duration and average vaccination coverage for 10 fixed post and 10 mobile teams',
+         x = 'Campaign duration', 
+         y = 'Vaccination coverage') +
+    theme_minimal()
+
+ggsave(filename = './figures/fig4a_cases_averted.pdf', 
+       plot = fig4a_cases_averted, 
+       device = 'pdf'
+       )
+
