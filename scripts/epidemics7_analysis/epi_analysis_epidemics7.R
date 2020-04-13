1eb4ae7b33ca539665fc64f688ea94bdcb5216a6
James Mba Azam <jamesazam@sun.ac.za>
2020-04-13 21:45:37 +0200

    this script might not run due to some changes to the called functions. Needs to be fixed!

--- scripts/epidemics7_analysis/epi_analysis_epidemics7.R
+++ scripts/epidemics7_analysis/epi_analysis_epidemics7.R
@@ -19,6 +19,7 @@ source('./scripts/parameters.R') #global parameter list
 source('./scripts/analyses_parameters.R') #specific parameter list for this analysis
 source('./scripts/epidemics7_analysis/scenarios.R') 
 source('./scripts/epidemics7_analysis/simulation_params.R')
+source('./scripts/measles_functions.R')
 
 
 ## Supply chain data ----
@@ -52,17 +53,17 @@ orv_far_pop_dynamics <- vector('list', nrow(sc_results))
 
 #strategy_name <- vector('list', nrow(sc_results))
 
-for (sc_result_row in 1: nrow(sc_results_final)) {
+for (sc_result_row in 1: nrow(sc_results_full)) {
     
     
     tp <- as.numeric(sc_model_params$vax_rate['mobile_team']) #if all mobile teams can exceed the doses needed to achieve the average daily performance, then it's safe to assume all teams can hit the average team performance
     orv_far_pop_dynamics[[sc_result_row]] <- runSimulations(
         R0 = orv_model_params$far_pop_R0 # transmission coeficient
         , run_time = orv_model_params$model_time # 1 yr!
-        , pop = initializePop(N = as.numeric(sc_results_final[sc_result_row, 'far_pop']), initPropImmune = 0.25, I0 = 1)
-        , strategy_name = sc_results_final[sc_result_row, 'strategy']
-        , vaxDay = as.numeric(sc_results_final[sc_result_row, 'campaign_start_compounded'])
-        , orv_duration = as.numeric(sc_results_final[sc_result_row ,'mt_team_days']) #for now we're only looking at the far campaigns 
+        , pop = initializePop(N = as.numeric(sc_results_full[sc_result_row, 'far_pop']), initPropImmune = 0.25, I0 = 1)
+        , strategy_name = sc_results_full[sc_result_row, 'strategy']
+        , vaxDay = as.numeric(sc_results_full[sc_result_row, 'campaign_start_compounded'])
+        , orv_duration = as.numeric(sc_results_full[sc_result_row ,'mt_team_days']) #for now we're only looking at the far campaigns 
         , n_team_type = 1
         , vax_eff = orv_model_params$vaccine_efficacy
         , team_performance = tp
