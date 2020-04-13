90d75246ffe86d268f56a1d78b9e3dea5f806836
James Mba Azam <jamesazam@sun.ac.za>
2020-04-13 21:41:23 +0200

    changed the model run time to 200 days from 365 days

--- scripts/analyses_parameters.R
+++ scripts/analyses_parameters.R
@@ -25,6 +25,7 @@ sc_model_params <- list(ambient_temp = c("below 40", "above 40")
                 , monodose_ovw_fixed_team = 0 #ovw = Open Vial Wastage: value between 0 and 100;
                 , monodose_ovw_mobile_team = 0 #ovw = Open Vial Wastage: value between 0 and 100; I'm assuming far campaigns have twice the near campaign wastage
                 , vax_rate = c(fixed_team = 450, mobile_team = 250)
+                , rcw25_ice_replacement_days = c(2, 3)
                 , site_campaign_dur_constraint = 10
                 )
 
@@ -32,7 +33,7 @@ sc_model_params <- list(ambient_temp = c("below 40", "above 40")
 orv_model_params <- list(near_pop_R0 = seq(2, 18, 0.5)
                          , far_pop_R0 = seq(2, 18, 0.5)
                          , vaccine_efficacy = 1
-                         , model_time = 365*5 #run model for 5 years
+                         , model_time = 200 #run model for 5 years
                          , LP = 7 #LP = Latent period
                          , IP = 7 #IP = Infectious period
                          , immune_response_timing = 7
