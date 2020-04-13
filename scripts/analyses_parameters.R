ad025d4476a52044d6435e56e9f708b649fbfdc9
James Mba Azam <jamesazam@sun.ac.za>
2020-04-13 21:40:58 +0200

    changed the mobile team wastage to 15% from 30% for consistency

--- scripts/analyses_parameters.R
+++ scripts/analyses_parameters.R
@@ -21,7 +21,7 @@ sc_model_params <- list(ambient_temp = c("below 40", "above 40")
                 , monodose_vial_vol = 21.09
                 , dose10_vial_vol = c(2.1, 3)
                 , dose10_ovw_fixed_team = 15 #ovw = Open Vial Wastage: value between 0 and 100;
-                , dose10_ovw_mobile_team = 30 #ovw = Open Vial Wastage: value between 0 and 100; I'm assuming far campaigns have twice the near campaign wastage
+                , dose10_ovw_mobile_team = 15 #ovw = Open Vial Wastage: value between 0 and 100; I'm assuming far campaigns have twice the near campaign wastage
                 , monodose_ovw_fixed_team = 0 #ovw = Open Vial Wastage: value between 0 and 100;
                 , monodose_ovw_mobile_team = 0 #ovw = Open Vial Wastage: value between 0 and 100; I'm assuming far campaigns have twice the near campaign wastage
                 , vax_rate = c(fixed_team = 450, mobile_team = 250)
