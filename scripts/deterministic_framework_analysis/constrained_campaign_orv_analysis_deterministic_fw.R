69d946c6a1098f0df415de574b2dc72a84d9f190
James Mba Azam <jamesazam@sun.ac.za>
2020-04-18 16:17:49 +0200

    transmission dynamics only script: moved the plotting code to other scripts

--- scripts/deterministic_framework_analysis/constrained_campaign_orv_analysis_deterministic_fw.R
+++ scripts/deterministic_framework_analysis/constrained_campaign_orv_analysis_deterministic_fw.R
@@ -1,21 +1,6 @@
 #global controls
 options(nwarnings = 10000) #print this many messages if they exist or occur
 
-#packages
-library('ggplot2')
-library('ggthemes')
-library('conflicted')
-library('dplyr')
-library('reshape2')
-library('tidyr')
-library('purrr')
-
-## Resolve package conflicts ####
-conflict_prefer('filter', 'dplyr') #anytime I call the function 'filter', I mean dplyr::filter
-conflict_prefer('select', 'dplyr')
-conflict_prefer('merge', 'base')
-conflict_prefer('summarise', 'dplyr')
-
 #helper functions and parameters
 source('./scripts/parameters.R') #global parameter list
 source('./scripts/analyses_parameters.R') #specific parameter list for this analysis
@@ -60,9 +45,11 @@ orv_model_inputs <- sc_results_full %>%
            )
 
 
+################################################################################
+#' ORV Scenarios
+################################################################################
 
-
-#' near locations' dynamics following orv ----
+#' near locations' dynamics following orv ====
 orv_near_dynamics <- tibble()
 
 for (sc_result_row in 1: nrow(orv_model_inputs)) {
@@ -85,77 +72,46 @@ for (sc_result_row in 1: nrow(orv_model_inputs)) {
     orv_near_dynamics <- rbind(orv_near_dynamics, near_orv_sim)
 }
 
-saveRDS(orv_near_dynamics, './model_output/deterministic_framework_analysis_output/orv_near_dynamics.rds')
-
-#Plot of transmission dynamics for near pop
-orv_near_dynamics_plot <- ggplot(data = orv_near_dynamics %>% filter(time <= 120)) + 
-    geom_line(aes(x = time, 
-                  y = S
-    ), 
-    color = 'blue', 
-    size = 1
-    ) + 
-    geom_line(aes(x = time, 
-                  y = I), 
-              color = 'red', 
-              size = 1) + 
-    labs(x = 'Time (Days)', 
-         y = 'Proportion of individuals', 
-         title = 'Transmission dynamics following orv in near locations'
-    ) + 
-    facet_grid(strategy ~ mt_equip_type + location_id)
-
-plot(orv_near_dynamics_plot)
+#' save the results to file
+saveRDS(orv_near_dynamics, 
+        './model_output/deterministic_framework_analysis_output/orv_near_dynamics_proportions.rds'
+        )
 
 
 
-#' far locations' dynamics following orv ----
+#' far locations' dynamics following orv ====
 orv_far_dynamics <- tibble()
+
 for (sc_result_row in 1: nrow(orv_model_inputs)) {
-    far_orv_sim <- run_orv_model(
-        strategy = orv_model_inputs[[sc_result_row, 'strategy']],
-        R0 = orv_model_params$far_pop_R0[21], # transmission coefficient
-        I0 = 1,
-        max_time = orv_model_params$model_time, 
-        target_pop_size = orv_model_inputs[[sc_result_row, 'far_pop']],
-        mt_equip_type = orv_model_inputs[[sc_result_row, 'mt_equip_type']],
-        vax_day = orv_model_inputs[[sc_result_row, 'mt_compounded_delay']],
-        latent_period = orv_model_params$LP,
-        infectious_period = orv_model_params$IP,
-        scenario_campaign_duration = orv_model_inputs[[sc_result_row ,'mt_dur_constrained']], 
-        location_id = orv_model_inputs[[sc_result_row, 'location_id']],
-        vax_efficacy = orv_model_params$vaccine_efficacy,
-        scenario_coverage = orv_model_inputs[[sc_result_row, 'mt_cov']],
-        browse = F
-    ) 
-    orv_far_dynamics <- rbind(orv_far_dynamics, far_orv_sim)
+  far_orv_sim <- run_orv_model(
+    strategy = orv_model_inputs[[sc_result_row, 'strategy']],
+    R0 = orv_model_params$far_pop_R0[21], # transmission coefficient
+    I0 = 1,
+    max_time = orv_model_params$model_time, 
+    target_pop_size = orv_model_inputs[[sc_result_row, 'far_pop']],
+    mt_equip_type = orv_model_inputs[[sc_result_row, 'mt_equip_type']],
+    vax_day = orv_model_inputs[[sc_result_row, 'mt_compounded_delay']],
+    latent_period = orv_model_params$LP,
+    infectious_period = orv_model_params$IP,
+    scenario_campaign_duration = orv_model_inputs[[sc_result_row ,'mt_dur_constrained']], 
+    location_id = orv_model_inputs[[sc_result_row, 'location_id']],
+    vax_efficacy = orv_model_params$vaccine_efficacy,
+    scenario_coverage = orv_model_inputs[[sc_result_row, 'mt_cov']],
+    browse = F
+  ) 
+  orv_far_dynamics <- rbind(orv_far_dynamics, far_orv_sim)
 }
 
-saveRDS(orv_far_dynamics, './model_output/deterministic_framework_analysis_output/ottar_seir_model_output/orv_far_dynamics.rds')
-
-#Plot of transmission dynamics for far pops ---
-orv_far_dynamics_plot <- ggplot(data = orv_far_dynamics) + 
-    geom_line(aes(x = time, 
-                  y = S
-                  ), 
-              color = 'blue', 
-              size = 1
-              ) + 
-    geom_line(aes(x = time, 
-                  y = I), 
-              color = 'red', 
-              size = 1) + 
-    labs(x = 'Time (Days)', 
-         y = 'Proportion of individuals', 
-         title = 'Transmission dynamics following orv in far locations'
-         ) + 
-    facet_grid(strategy ~ mt_equip_type + location_id)
-
-plot(orv_far_dynamics_plot)
+#' save the results to file
+saveRDS(orv_far_dynamics, 
+        './model_output/deterministic_framework_analysis_output/orv_far_dynamics_proportions.rds'
+        )
 
 
 
-#' Baseline: no orv ---
+################################################################################
+#' BASELINE: no orv ---
+################################################################################
 
 #' near locations ====
 no_orv_near_dynamics <- tibble()
@@ -164,6 +120,7 @@ for (location in 1: nrow(site_pops_df)) {
     no_orv_near_sim <- run_orv_model(
         strategy = 'no_orv_near_pops',
         R0 = orv_model_params$near_pop_R0[21], # transmission coefficient
+        I0 = 1,
         max_time = orv_model_params$model_time, 
         target_pop_size = site_pops_df[[location, 'near_pop']],
         mt_equip_type = 'none',
@@ -179,26 +136,12 @@ for (location in 1: nrow(site_pops_df)) {
     no_orv_near_dynamics <- rbind(no_orv_near_dynamics, no_orv_near_sim)
 }
 
+#' save the results to file
+saveRDS(no_orv_near_dynamics, 
+        './model_output/deterministic_framework_analysis_output/no_orv_near_dynamics_proportions.rds'
+        )
 
-#Plot of transmission dynamics for far pops (no orv) ---
-no_orv_near_dynamics_plot <- ggplot(data = no_orv_near_dynamics) + 
-    geom_line(aes(x = time, 
-                  y = S
-    ), 
-    color = 'blue', 
-    size = 2
-    ) + 
-    geom_line(aes(x = time, 
-                  y = I), 
-              color = 'red', 
-              size = 2) + 
-    labs(x = 'Time (Days)', 
-         y = 'Proportion of individuals', 
-         title = 'Transmission dynamics of near locations (No ORV)'
-    ) + 
-    facet_grid( ~ location_id)
 
-plot(no_orv_near_dynamics_plot)
 
 
 #' far locations ====
@@ -208,6 +151,7 @@ for (location in 1: nrow(site_pops_df)) {
     no_orv_far_sim <- run_orv_model(
         strategy = 'no_orv_far_pops',
         R0 = orv_model_params$far_pop_R0[21], # transmission coefficient
+        I0 = 1,
         max_time = orv_model_params$model_time, 
         target_pop_size = site_pops_df[[location, 'far_pop']],
         mt_equip_type = 'none',
@@ -223,158 +167,8 @@ for (location in 1: nrow(site_pops_df)) {
     no_orv_far_dynamics <- rbind(no_orv_far_dynamics, no_orv_far_sim)
 }
 
+#' save the results to file
+saveRDS(no_orv_far_dynamics, 
+        './model_output/deterministic_framework_analysis_output/no_orv_far_dynamics_proportions.rds'
+        )
 
-#Plot of transmission dynamics for far pops (no orv) ---
-no_orv_far_dynamics_plot <- ggplot(data = no_orv_far_dynamics) + 
-    geom_line(aes(x = time, 
-                  y = S
-    ), 
-    color = 'blue', 
-    size = 2
-    ) + 
-    geom_line(aes(x = time, 
-                  y = I), 
-              color = 'red', 
-              size = 2) + 
-    labs(x = 'Time (Days)', 
-         y = 'Proportion of individuals', 
-         title = 'Transmission dynamics of far locations  (No ORV)'
-    ) + 
-    facet_grid( ~ location_id)
-
-plot(no_orv_far_dynamics_plot)
-
-
-#' Plots (Monodose fcc for illustration) ----
-
-#' 1a. Outbreak size: by near locations ====
-monodose_fcc_near_outbreak_size_plot <- orv_near_dynamics %>% 
-    group_by(strategy, mt_equip_type) %>% 
-  #  filter(time == orv_model_params$model_time) %>% 
-    filter(time == orv_model_params$model_time, strategy == 'monodose_fcc') %>% 
-    ggplot(aes(x = time, y = K)) + 
-    geom_bar(stat = 'identity') + 
-    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) + 
-    labs(x = 'Location', y = 'Outbreak size', title = 'Outbreak sizes by location and equipment used (Near Locations)') +
-    facet_grid(strategy ~ mt_equip_type + location_id)
-
-plot(monodose_fcc_near_outbreak_size_plot)
-
-#' 1b. Incidence: by near locations
-monodose_fcc_near_incidence_plot <- orv_near_dynamics %>% 
-  group_by(strategy, mt_equip_type) %>% 
-  #  filter(time == orv_model_params$model_time) %>% 
-  filter(strategy == 'monodose_fcc') %>% 
-  ggplot(aes(x = time, y = I)) + 
-  geom_line(size = 2, color = 'red') + 
-  labs(x = 'Time (Days)', y = 'Incidence', title = 'Monodose FCC incidence by location and equipment used (Near Locations)') +
-  facet_grid(strategy ~ mt_equip_type + location_id)
-
-plot(monodose_fcc_near_incidence_plot)
-
-#' 2a. Outbreak size: by far locations
-monodose_fcc_far_outbreak_size_plot <- orv_far_dynamics %>% 
-    group_by(strategy, mt_equip_type) %>% 
-  #  filter(time == orv_model_params$model_time) %>% 
-    filter(time == orv_model_params$model_time, strategy == 'monodose_fcc') %>% 
-    ggplot(aes(x = time, y = K)) + 
-    geom_bar(stat = 'identity') + 
-    theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) + 
-    labs(x = 'Location', y = 'Outbreak size', title = 'Monodose FCC outbreak sizes per location and equipment used (Far Locations)') +
-    facet_grid(strategy ~ mt_equip_type + location_id)
-
-plot(monodose_fcc_far_outbreak_size_plot)
-
-
-#' 2b. Incidence: by far locations
-monodose_fcc_far_incidence_plot <- orv_far_dynamics %>% 
-  group_by(strategy, mt_equip_type) %>% 
-  #  filter(time == orv_model_params$model_time) %>% 
-  filter(strategy == 'monodose_fcc') %>% 
-  ggplot(aes(x = time, y = I)) + 
-  geom_line(size = 2, color = 'red') + 
-  labs(x = 'Time (Days)', y = 'Incidence', title = 'Monodose FCC incidence by location and equipment used (Far Locations)') +
-  facet_grid(strategy ~ mt_equip_type + location_id)
-
-plot(monodose_fcc_far_incidence_plot)
-
-
-#' print the monodose data to support the explanation of the plots:
-# library(xtable)
-# monodose_sc_results <- orv_model_inputs %>% 
-#   filter(strategy == 'monodose_fcc') %>% 
-#   select(-c(near_pop, far_pop, ft_dur_constrained, mt_dur_constrained)) 
-# 
-#   print(xtable(monodose_sc_results), 
-#       floating=FALSE, 
-#       latex.environments = NULL, 
-#       booktabs = TRUE
-#       )
-
-
-
-#' total outbreak size across all near locations (orv) ----
-#' near pop (orv) ====
-orv_near_dynamics_outbreak_size_df <- orv_near_dynamics %>% 
-    group_by(strategy, mt_equip_type) %>% 
-    filter(time == orv_model_params$model_time) %>% 
-    summarise(orv_near_total_cases = sum(K))
-
-#' far pop (orv) ====
-orv_far_dynamics_outbreak_size_df <- orv_far_dynamics %>% 
-    group_by(strategy, mt_equip_type) %>% 
-    filter(time == orv_model_params$model_time) %>% 
-    summarise(orv_far_total_cases = sum(K))
-
-#' total outbreak size across all near locations (baseline; no orv) ----
-#' near pop (no orv) ====
-no_orv_near_dynamics_outbreak_size <- no_orv_near_dynamics %>% 
-    filter(time == orv_model_params$model_time) %>% 
-    summarise(total_cases = sum(K)) %>% 
-    as.numeric()
-
-no_orv_near_dynamics_outbreak_size
-
-#' far pop (no orv) ====
-no_orv_far_dynamics_outbreak_size <- no_orv_far_dynamics %>% 
-    filter(time == orv_model_params$model_time) %>% 
-    summarise(total_cases = sum(K)) %>% 
-    as.numeric()
-
-no_orv_far_dynamics_outbreak_size
-
-
-# calculate the total outbreak size across all locations, near and far
-orv_outbreak_sizes_aggregated <- left_join(orv_near_dynamics_outbreak_size_df, 
-                                orv_far_dynamics_outbreak_size_df
-                                ) %>% 
-    mutate(orv_total_cases = orv_near_total_cases + orv_far_total_cases)
-
-#' cases averted ====
-#' 
-cases_averted_df <- orv_outbreak_sizes_aggregated %>% 
-    mutate(cases_averted = (no_orv_near_dynamics_outbreak_size + no_orv_far_dynamics_outbreak_size) - orv_total_cases)
-
-cases_averted_df
-
-saveRDS(cases_averted_df, './model_output/deterministic_framework_analysis_output/cases_averted_deterministic_seir.rds')
-
-
-
-#' Combine all simulations into a FINAL data.frame for post-processing
-
-#'load supply chain summary
-#'
-sc_results_summary_10_teams <- readRDS('./model_output/sc_results_summary_10_teams.rds') %>% 
-    as_tibble() %>% 
-    rename(mt_equip_type = mt_equip) %>% 
-    mutate(strategy = as_factor(gsub(pattern = '_parallel', replacement = '', .$strategy))) #shorten the strategy names
-
-sc_epi_analysis_summary_10_teams <- left_join(sc_results_summary_10_teams , 
-                                              cases_averted_df, 
-                                              by = c('strategy', 'mt_equip_type'
-                                              )
-                                              )
-
-saveRDS(sc_epi_analysis_summary_10_teams, file = './model_output/deterministic_framework_analysis_output/sc_epi_analysis_summary_10_teams.rds')
-View(sc_epi_analysis_summary_10_teams)
