c12b64251090cc9ca75f57b3191b9c9407daffdf
James Mba Azam <jamesazam@sun.ac.za>
2020-04-18 16:16:37 +0200

    script to make cases averted bar plot

--- scripts/deterministic_framework_analysis/fig_cases_averted.R
+++ scripts/deterministic_framework_analysis/fig_cases_averted.R
@@ -1,31 +1,60 @@
 library(ggplot2)
 library(ggthemes)
+library(stringr)
+library(forcats)
+library(dplyr)
 
-#laod plotting data
-cases_averted_plot_df <- readRDS('./model_output/sc_epi_analysis_summary_10_teams.rds')
+#load plotting data
+cases_averted_df <- readRDS("./model_output/deterministic_framework_analysis_output/sc_epi_analysis_summary_10_teams.rds") %>%
+    mutate(
+        cold_chain = as_factor(ifelse(str_detect(strategy, "_fcc"),
+                                      "cc",
+                                      ifelse(str_detect(strategy, "mixed_"),
+                                             "part_cc",
+                                             "no_cc"
+                                      )
+        )),
+        vial_type = as_factor(ifelse(str_detect(strategy, "dose10_"),
+                                     "dose10",
+                                     ifelse(str_detect(strategy, "mixed_"),
+                                            "dose10 + monodose",
+                                            "monodose"
+                                     )
+        ))
+    )
 
 #' visualisations
-fig4a_cases_averted <- ggplot(data = cases_averted_plot_df) + 
-    geom_text(aes(x = campaign_duration,
-                  y = average_coverage,
-                  label = round(cases_averted, 2)
-                  ),
-               check_overlap = TRUE,
-              nudge_y = 0.01
-              ) +
-    geom_jitter(aes(x = campaign_duration, 
-                    y = average_coverage, 
-                    color = strategy,
-                    size = cases_averted), 
-                width = 0.15#, 
-               # height = 0.025
-                ) +
-    labs(title = 'Ranking of scenarios by cases averted, expected vaccination coverage, and total operational duration',
-         x = 'Campaign duration', 
-         y = 'Vaccination coverage') +
+cases_averted_plot <- ggplot(data = cases_averted_df,
+                             aes(x = cold_chain,
+                                 y = cases_averted
+                                 )
+                             ) + 
+    geom_col(aes(fill = cold_chain,
+                 linetype = mt_equip_type, 
+                 color = vial_type),
+             size = 1,
+             position = position_dodge2(preserve = 'single')
+    ) + 
+  scale_linetype_manual(name = 'Mobile team equipment',
+                        values = c('solid', 'twodash')) +
+  scale_fill_brewer(palette = 'Set3', 
+                    name = 'Cold chain use', 
+                    breaks = c('cc', 
+                               'no_cc', 
+                               'part_cc'),
+                    labels = c('Cold chain' , 
+                               'Out of Cold Chain', 
+                               'Part Cold Chain' )) + 
+  scale_color_brewer(name = 'Vial type', 
+                     palette = 'Dark2') + 
+  guides(color = guide_legend(override.aes = list(fill = NA)),
+         linetype = guide_legend(override.aes = list(fill = NA, col = 'black'))) +
+    labs(title = 'Cases averted by mobile team equipment, vial type, and cold chain decision',
+         x = 'Strategy', 
+         y = 'Cases averted (thousands)') +
     theme_minimal()
 
-plot(fig4a_cases_averted)
+plot(cases_averted_plot)
 
 # ggsave(filename = './figures/fig4a_cases_averted.pdf', 
 #        plot = fig4a_cases_averted, 
