811a1a60010f12cbcf20b2746b0e7df398e3ac43
James Mba Azam <jamesazam@sun.ac.za>
2020-08-27 02:14:42 +0200

    a script to source all the analysis files and run them to produce results

---
+++ scripts/deterministic_framework_analysis/run_full_analysis.R
@@ -0,0 +1,9 @@
+#'This script is for running my analysis from scratch after I've specified the parameters in the
+#'individual scripts
+
+source('./scripts/deterministic_framework_analysis/constrained_campaign_sc_analysis_deterministic_fw.R')
+source('./scripts/deterministic_framework_analysis/constrained_campaign_orv_analysis_deterministic_fw.R')
+source('./scripts/deterministic_framework_analysis/outbreak_sizes_cases_averted_calculation.R')
+source('./scripts/deterministic_framework_analysis/fig_coverage_vs_duration_scenario_ranking.R')
+source('./scripts/deterministic_framework_analysis/fig_coverage_vs_duration_scenario_ranking.R')
+source('./scripts/deterministic_framework_analysis/fig_cases_averted.R')
