aa53736ec18d1269b61b73f169041b5f38ae50dd
James Mba Azam <jamesazam@sun.ac.za>
2020-11-14 01:41:16 +0200

    moved the plotting code to a new script and removed some redundant code

--- scripts/deterministic_framework_analysis/sensitivity_analyses/pop_size/pop_sizes_prop_teams_sc_sensitivity.R
+++ scripts/deterministic_framework_analysis/sensitivity_analyses/pop_size/pop_sizes_prop_teams_sc_sensitivity.R
@@ -111,10 +111,7 @@ campaign_metrics_pop_size_prop_teams_sensitivity_trunc <- campaign_metrics_pop_s
 pop_size_prop_teams_sensitivity_sc_analysis_full <- bind_cols(
     campaign_delay_pop_size_prop_teams_sensitivity_trunc,
     campaign_metrics_pop_size_prop_teams_sensitivity_trunc
-    ) %>% mutate(
-        n_teams_fixed = sim_params_pop_size_prop_teams_sensitivity$n_teams_fixed,
-        n_teams_mobile = sim_params_pop_size_prop_teams_sensitivity$n_teams_mobile
-    )
+    ) 
 
 
 #' Epi input: compounded delays
@@ -142,16 +139,16 @@ sc_analysis_pop_size_prop_teams_sensitivity_summary <- sc_analysis_pop_size_prop
               campaign_duration = sum(site_campaign_dur_constrained) + campaign_start[1]
               )%>%
     mutate(
-        cold_chain = as_factor(ifelse(str_detect(strategy, "_fcc"),
+        cold_chain = as_factor(ifelse(stringr::str_detect(strategy, "_fcc"),
                                       "cc",
-                                      ifelse(str_detect(strategy, "mixed_"),
+                                      ifelse(stringr::str_detect(strategy, "mixed_"),
                                              "part_cc",
                                              "no_cc"
                                       )
         )),
-        vial_type = as_factor(ifelse(str_detect(strategy, "dose10_"),
+        vial_type = as_factor(ifelse(stringr::str_detect(strategy, "dose10_"),
                                      "dose10",
-                                     ifelse(str_detect(strategy, "mixed_"),
+                                     ifelse(stringr::str_detect(strategy, "mixed_"),
                                             "dose10 + monodose",
                                             "monodose"
                                      )
@@ -160,90 +157,6 @@ sc_analysis_pop_size_prop_teams_sensitivity_summary <- sc_analysis_pop_size_prop
 
 
 
-#A quick plot
-#beeswarm plot, a better option than jitter plots
-library(ggbeeswarm)
-
-coverage_duration_plot_plain_shapes_beeswarm <- ggplot(data = sc_analysis_pop_size_prop_teams_sensitivity_summary, 
-                                                       aes(x = campaign_duration, 
-                                                           y = average_coverage)) + 
-    geom_beeswarm(groupOnX = F,
-                  aes(shape = mt_equip_type, 
-                      fill = vial_type,
-                      color = cold_chain
-                  ),
-                  size = 4,
-                  cex = 5, 
-                  stroke = 2
-    ) +
-    scale_y_continuous(breaks = seq(min(sc_analysis_pop_size_prop_teams_sensitivity_summary$average_coverage), 
-                                    max(sc_analysis_pop_size_prop_teams_sensitivity_summary$average_coverage), 
-                                    length.out = 5),
-                       labels = percent(seq(min(sc_analysis_pop_size_prop_teams_sensitivity_summary$average_coverage), 
-                                            max(sc_analysis_pop_size_prop_teams_sensitivity_summary$average_coverage), 
-                                            length.out = 5)
-                       )
-    ) +
-    scale_x_continuous(breaks = seq(min(sc_analysis_pop_size_prop_teams_sensitivity_summary$campaign_duration), 
-                                    max(sc_analysis_pop_size_prop_teams_sensitivity_summary$campaign_duration), 
-                                    length.out = 5
-    ),
-    labels = seq(min(sc_analysis_pop_size_prop_teams_sensitivity_summary$campaign_duration), 
-                 max(sc_analysis_pop_size_prop_teams_sensitivity_summary$campaign_duration), 
-                 length.out = 5)
-    ) +
-    scale_shape_manual(name = 'Mobile team equipment', 
-                       values = c(21, 24), 
-                       labels = c('rcw25' = 'RCW25', 
-                                  'vaxCarr' = 'Vaccine carrier')
-    ) +
-    scale_color_manual(name = 'Cold chain option', 
-                       breaks = c('cc', 
-                                  'no_cc', 
-                                  'part_cc'),
-                       labels = c('Cold chain' , 
-                                  'Outside cold chain', 
-                                  'Partial cold chain' ),
-                       values = c('cc' = '#00AFBB', 
-                                  'no_cc' = '#FC4E07', 
-                                  'part_cc' = '#E7B800')
-    ) +
-    scale_fill_manual(name = 'Vial type',
-                      breaks = c('dose10', 
-                                 'monodose',
-                                 'dose10 + monodose'),
-                      values = c('dose10' = NA, 
-                                 'monodose' = NA,
-                                 'dose10 + monodose' = NA),
-                      labels = c('dose10' = '10-dose', 
-                                 'monodose' = 'Monodose',
-                                 'dose10 + monodose' = '10-dose & Monodose')
-    ) +
-    guides(shape = guide_legend(override.aes = list(size = 6, 
-                                                    stroke = 1.2
-    ), 
-    order = 1
-    ), 
-    fill = guide_legend(override.aes = list(size = 6, 
-                                            color = 'black', 
-                                            fill = 'white', 
-                                            shape = 22)
-    )
-    ) +
-    labs(
-        title = 'Strategy ranking by vaccination coverage and campaign duration (Proportional team type allocation)',
-        x = "Campaign duration (days)",
-        y = "Vaccination coverage"
-        ) +
-    # ggpubr::font('xy.text', face = 'plain') +
-    # ggpubr::font('xy.title', face = 'plain') +
-    # ggpubr::theme_pubr(legend = 'right', 
-    #                    base_size = 16,
-    #                    border = T) + 
-    facet_wrap(n_teams_fixed + near_pop ~ n_teams_mobile + far_pop) +
-    NULL
-
-plot(coverage_duration_plot_plain_shapes_beeswarm)
 
 
  #Save the final analysis
