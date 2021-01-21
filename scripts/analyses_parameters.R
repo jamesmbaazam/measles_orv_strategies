c6e96103048d78001a58aea1121fac91a44aba2e
James Mba Azam <jamesazam@sun.ac.za>
2021-01-21 22:37:33 +0200

    changed the vaccine efficacy value to 0.84 with reference to the WHO vaccine measles paper.

--- scripts/analyses_parameters.R
+++ scripts/analyses_parameters.R
@@ -34,7 +34,7 @@ sc_model_params <- list(ambient_temp = c("below 40", "above 40")
 
 orv_model_params <- list(near_pop_R0 = seq(2, 18, 0.5)
                          , far_pop_R0 = seq(2, 18, 0.5)
-                         , vaccine_efficacy = 1
+                         , vaccine_efficacy = 0.84 #source = WHO measles vaccine position paper
                          , model_time = 365*5 #run model for 5 years
                          , LP = 7 #LP = Latent period
                          , IP = 7 #IP = Infectious period
