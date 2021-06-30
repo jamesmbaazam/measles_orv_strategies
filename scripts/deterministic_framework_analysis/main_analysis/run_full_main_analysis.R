100ccb89c75e5e58af6c657f79f391bf75d9012a
James Mba Azam <jamesazam@sun.ac.za>
2021-06-30 20:10:54 +0200

    renamed some variables and obects from msf params to main analysis

--- scripts/deterministic_framework_analysis/main_analysis/run_full_main_analysis.R
+++ scripts/deterministic_framework_analysis/main_analysis/run_full_main_analysis.R
@@ -3,7 +3,7 @@
 
 source('./scripts/deterministic_framework_analysis/constrained_campaign_sc_analysis_deterministic_fw.R')
 source('./scripts/deterministic_framework_analysis/constrained_campaign_orv_analysis_deterministic_fw.R')
-source('./scripts/deterministic_framework_analysis/outbreak_sizes_cases_averted_calculation.R')
+source('./scripts/deterministic_framework_analysis/main_analysis/cases_averted_calculation_main_analysis.R')
 source('./scripts/deterministic_framework_analysis/fig_coverage_vs_duration_scenario_ranking.R')
 source('./scripts/deterministic_framework_analysis/fig_coverage_vs_duration_scenario_ranking.R')
 source('./scripts/deterministic_framework_analysis/fig_cases_averted.R')
