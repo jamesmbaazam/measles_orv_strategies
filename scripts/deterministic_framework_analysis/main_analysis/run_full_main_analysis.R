fae5d6757a088254affd87d35a144c2a87f5b5d6
James Mba Azam <jamesazam@sun.ac.za>
2021-07-01 16:49:10 +0200

    Added comments to the script

--- scripts/deterministic_framework_analysis/main_analysis/run_full_main_analysis.R
+++ scripts/deterministic_framework_analysis/main_analysis/run_full_main_analysis.R
@@ -1,9 +1,20 @@
-#'This script is for running my analysis from scratch after I've specified the parameters in the
-#'individual scripts
+#'This script sources the scripts and runs the individual components of the modelling framework,
+#'combines the results, performs aggregations, saves the output to file, and makes plots
 
-source('./scripts/deterministic_framework_analysis/constrained_campaign_sc_analysis_deterministic_fw.R')
-source('./scripts/deterministic_framework_analysis/constrained_campaign_orv_analysis_deterministic_fw.R')
+#'Step 1. Run the supply chain(sc) analyses and save the results to file
+source('./scripts/deterministic_framework_analysis/main_analysis/constrained_campaign_sc_main_analysis.R')
+
+#'Step 2. Run the epidemiological(epi) model using some of the supply chain outputs and save the results to file
+source('./scripts/deterministic_framework_analysis/main_analysis/constrained_campaign_orv_main_analysis.R')
+
+#'Step 3. Combine the supply chain and epi results and calculate the cases averted and save to file
 source('./scripts/deterministic_framework_analysis/main_analysis/cases_averted_calculation_main_analysis.R')
-source('./scripts/deterministic_framework_analysis/fig_coverage_vs_duration_scenario_ranking.R')
-source('./scripts/deterministic_framework_analysis/fig_coverage_vs_duration_scenario_ranking.R')
-source('./scripts/deterministic_framework_analysis/fig_cases_averted.R')
+
+#'Step 4. Plot the cases averted from the main analysis (pre-deployment delay of 21 days)
+source('./scripts/deterministic_framework_analysis/main_analysis/fig_cases_averted_main_analysis.R')
+
+#'Step 5. Plot the vaccination coverage vrs campaign duration from the main analysis (pre-deployment delay of 21 days)
+source('./scripts/deterministic_framework_analysis/main_analysis/fig_coverage_vs_duration_scenario_ranking_main_analysis.R')
+
+
+
