511c2ff5d75d33088cb98085e11ad2be0ae4412e
James Azam <jamesazam@sun.ac.za>
2022-01-20 12:23:48 +0200

    Remove redundant code, fix the y-axis scale, and add a horizontal line at 0

--- scripts/deterministic_framework_analysis/main_analysis/fig_cases_averted_main_analysis.R
+++ scripts/deterministic_framework_analysis/main_analysis/fig_cases_averted_main_analysis.R
@@ -131,31 +131,26 @@ relative_cases_averted_lollipop_plot <- cases_averted_df_cc_mod %>%
                                           'Part cold chain', 'Outside cold chain')
                    )
   ) +
-  scale_y_continuous(breaks = round(seq(min(cases_averted_df_cc_mod$relative_cases_averted), 
-                                  max(cases_averted_df_cc_mod$relative_cases_averted), 
-                                  1.5
-                                  ), 1),
-                     labels = round(seq(min(cases_averted_df_cc_mod$relative_cases_averted), 
-                                  max(cases_averted_df_cc_mod$relative_cases_averted), 
-                                  1.5
-                                  ), 1)
-                     ) +
-  guides(shape = guide_legend(override.aes = list(size = 6, stroke = 1.2)
+  scale_y_continuous(breaks = seq(-9, 2, 1),
+                     labels = seq(-9, 2, 1)
+                     ) + 
+    geom_hline(yintercept = 0, size = 1.2) +
+    guides(shape = guide_legend(override.aes = list(size = 6, stroke = 1.2)
                               ), 
          fill = guide_legend(override.aes = list(size = 6, 
                                           color = 'black', 
                                           fill = 'white', 
                                           shape = 22)
-  )
-  ) +
+                             )
+         ) +
   facet_grid(~ cold_chain, 
              scales = 'free_x', 
              switch = 'x', 
              labeller = labeller(cold_chain = c(cc = 'Full cold chain', 
                                                 part_cc = 'Partial cold chain', 
                                                 no_cc = 'Outside cold chain')
-             )
-  ) +
+                                 )
+             ) +
   labs(x = 'Strategy', 
        y = 'Cases averted (thousands)',
        fill = 'Vial type'
@@ -206,9 +201,10 @@ ggsave(plot = relative_cases_averted_lollipop_plot,
 #                      labels = c('Cold chain' , 
 #                                 'Outside cold chain', 
 #                                 'Partial cold chain' ),
-#                      values = c('cc' = 'seagreen3', 
-#                                 'no_cc' = 'dodgerblue3', 
-#                                 'part_cc' = 'purple3')) + 
+#                      values = c('cc' = '#00AFBB', 
+#                                 'no_cc' = '#FC4E07', 
+#                                 'part_cc' = '#E7B800')
+#   ) + 
 #   scale_fill_manual(name = 'Vial type',
 #                     breaks = c('dose10', 
 #                                'monodose',
@@ -224,253 +220,52 @@ ggsave(plot = relative_cases_averted_lollipop_plot,
 #                    labels = ifelse(cases_averted_df_cc_mod$cold_chain == 'cc',
 #                                    'Full cold chain',
 #                                    ifelse(cases_averted_df_cc_mod$cold_chain == 'part_cc',
-#                                           'Part cold chain', 'Outside cold chain'
-#                                    )
+#                                           'Part cold chain', 'Outside cold chain')
 #                    )
 #   ) +
-#   scale_y_continuous(breaks = seq(-45, 30, 5),
-#                      labels = seq(-45, 30, 5)
+#   scale_y_continuous(breaks = round(seq(0, 
+#                                         max(cases_averted_df_cc_mod$cases_averted)/1000, 
+#                                         15
+#                                         ), 1
+#                                     ),
+#   labels = round(seq(0, 
+#                      max(cases_averted_df_cc_mod$cases_averted)/1000, 
+#                      15), 1
+#                  )
 #   ) +
-#   guides(shape = guide_legend(override.aes = list(size = 6, stroke = 1.2), order = 1), 
-#          fill = guide_legend(override.aes = list(size = 6, 
-#                                                  color = 'black', 
-#                                                  fill = 'white', 
-#                                                  shape = 22)
-#          )
+#   guides(shape = guide_legend(override.aes = list(size = 6, 
+#                                                   stroke = 1.2
+#                                                   )
+#                               ), 
+#   fill = guide_legend(override.aes = list(size = 6, 
+#                                           color = 'black', 
+#                                           fill = 'white', 
+#                                           shape = 22)
+#                       )
 #   ) +
-#   labs(#title = 'Cases averted by mobile team equipment, vial type, and cold chain decision',
-#     x = 'Strategy', 
-#     y = 'Cases averted (thousands)'
-#   ) + 
-#   ggpubr::font('xy.text', face = 'plain') +
-#   ggpubr::font('xy.title', face = 'plain') +
-#   ggpubr::theme_pubr(legend = 'right', base_size = 18) +
+#   facet_grid(~ cold_chain, 
+#              scales = 'free_x', 
+#              switch = 'x', 
+#              labeller = labeller(cold_chain = c(cc = 'Full cold chain', 
+#                                                 part_cc = 'Partial cold chain', 
+#                                                 no_cc = 'Outside cold chain')
+#              )
+#   ) +
+#   labs(x = 'Strategy', 
+#        y = 'Cases averted (thousands)',
+#        fill = 'Vial type') + 
+#   theme_minimal(base_size = 18) +
 #   NULL
 # 
-# plot(cases_averted_lollipop_plot_with_package)
+# plot(cases_averted_lollipop_plot)
 # 
-# ggsave(filename = 'cases_averted_lollipop_plot_with_package_plain_shapes.eps', 
-#        plot = cases_averted_lollipop_plot_with_package, 
-#        device = "eps", 
-#        path = plot_path,
-#        width = 28.4,
-#        height = 21,
+# #save plot
+# ggsave(plot = cases_averted_lollipop_plot,
+#        filename = './figures/deterministic_framework_analysis_figures/main_analysis/fig5_cases_averted_main_analysis_lollipop_plot.jpg',
+#        width = 23.76,
+#        height = 17.86,
 #        units = 'cm'
-# )
-
-
-### Earlier options ----
-
-#' option 1a: label the x-axis with mobile team equip, use vial type as color border
-#' cases_averted_plot_option1a <- ggplot(data = cases_averted_df_mod_strategy,
-#'                              aes(x = reorder(strategy, cases_averted),
-#'                                  y = cases_averted/1000
-#'                              )
-#' ) + 
-#'   geom_col(aes(fill = cold_chain,
-#' #               linetype = mt_equip_type, 
-#'                color = vial_type),
-#'            size = 1,
-#'            position = position_dodge2(preserve = 'single')
-#'   ) + 
-#'  # scale_linetype_manual(name = 'Mobile team equipment',
-#'  #                       values = c('solid', 'twodash')) +
-#'   scale_fill_brewer(palette = 'Set3', 
-#'                     name = 'Cold chain option', 
-#'                     breaks = c('cc', 
-#'                                'no_cc', 
-#'                                'part_cc'),
-#'                     labels = c('Cold chain' , 
-#'                                'Outside cold chain', 
-#'                                'Partial cold chain' )) + 
-#'   scale_color_brewer(name = 'Vial type', 
-#'                      palette = 'Dark2') + 
-#'   scale_x_discrete(breaks = cases_averted_df_mod_strategy$strategy, 
-#'                    labels = cases_averted_df_mod_strategy$mt_equip_type) +
-#'   guides(color = guide_legend(override.aes = list(fill = NA)),
-#'          linetype = guide_legend(override.aes = list(fill = NA, col = 'black'))) +
-#'   labs(#title = 'Cases averted by mobile team equipment, vial type, and cold chain decision',
-#'        x = 'Mobile team equipment', 
-#'        y = 'Cases averted (thousands)') +
-#'   ggpubr::font('xy.text', face = 'plain') +
-#'   ggpubr::font('xy.title', face = 'plain') +
-#'   ggpubr::theme_pubr(legend = 'right', base_size = 18) +
-#'   NULL
-#' 
-#' plot(cases_averted_plot_option1a)
-#' 
-#' 
-#' 
-#' #' option 1b: label the x-axis with mobile team equip, use vial type as color border
-#' cases_averted_plot_option1b <- ggplot(data = cases_averted_df_mod_strategy,
-#'                                      aes(x = reorder(strategy, cases_averted),
-#'                                          y = cases_averted/1000
-#'                                      )
-#' ) + 
-#'   geom_col(aes(fill = cold_chain,
-#'                #               linetype = mt_equip_type, 
-#'                color = vial_type),
-#'            size = 1,
-#'            position = position_dodge2(preserve = 'single')
-#'   ) + 
-#'   # scale_linetype_manual(name = 'Mobile team equipment',
-#'   #                       values = c('solid', 'twodash')) +
-#'   scale_fill_brewer(palette = 'Set3', 
-#'                     name = 'Cold chain option', 
-#'                     breaks = c('cc', 
-#'                                'no_cc', 
-#'                                'part_cc'),
-#'                     labels = c('Cold chain' , 
-#'                                'Outside cold chain', 
-#'                                'Partial cold chain' )) + 
-#'   scale_color_brewer(name = 'Vial type', 
-#'                      palette = 'Dark2') + 
-#'   scale_x_discrete(breaks = cases_averted_df_mod_strategy$strategy, 
-#'                    labels = cases_averted_df_mod_strategy$mt_equip_type) +
-#'   guides(color = guide_legend(override.aes = list(fill = NA)),
-#'          linetype = guide_legend(override.aes = list(fill = NA, col = 'black'))) +
-#'   labs(#title = 'Cases averted by mobile team equipment, vial type, and cold chain decision',
-#'        x = 'Mobile team equipment', 
-#'        y = 'Cases averted (thousands)') +
-#'   coord_flip() +
-#'   ggpubr::font('xy.text', face = 'plain') +
-#'   ggpubr::font('xy.title', face = 'plain') +
-#'   ggpubr::theme_pubr(legend = 'right', base_size = 18) +
-#'   NULL
-#' 
-#' plot(cases_averted_plot_option1b)
-#' 
-#' 
-#' #' option 2a: flipped, label x-axis with vial type, use linetypes as borders
-#' #' 
-#' cases_averted_plot_option2a <- ggplot(data = cases_averted_df_mod_strategy,
-#'                                      aes(x = reorder(strategy, cases_averted),
-#'                                          y = cases_averted/1000
-#'                                      )
-#' ) + 
-#'   geom_col(aes(fill = cold_chain,
-#'                linetype = mt_equip_type),
-#'            color = 'black',
-#'            size = 1,
-#'            position = position_dodge2(preserve = 'single')
-#'   ) + 
-#'    scale_linetype_manual(name = 'Mobile team equipment',
-#'                          values = c('solid', 'twodash')) +
-#'   scale_fill_brewer(palette = 'Set3', 
-#'                     name = 'Cold chain option', 
-#'                     breaks = c('cc', 
-#'                                'no_cc', 
-#'                                'part_cc'),
-#'                     labels = c('Cold chain' , 
-#'                                'Outside cold chain', 
-#'                                'Partial cold chain' )) + 
-#'   scale_x_discrete(breaks = cases_averted_df_mod_strategy$strategy, 
-#'                    labels = cases_averted_df_mod_strategy$vial_type) +
-#'   guides(linetype = guide_legend(override.aes = list(fill = NA, col = 'black'))) +
-#'   labs(#title = 'Cases averted by mobile team equipment, vial type, and cold chain decision',
-#'        x = 'Vial type choice', 
-#'        y = 'Cases averted (thousands)') +
-#'   coord_flip() + 
-#'   ggpubr::font('xy.text', face = 'plain') +
-#'   ggpubr::font('xy.title', face = 'plain') +
-#'   ggpubr::theme_pubr(legend = 'right', base_size = 18) +
-#'   NULL
-#' 
-#' plot(cases_averted_plot_option2a)
-#' 
-#' 
-#' 
-#' 
-#' #' option 2b: label x-axis with vial type, use linetypes as borders
-#' #' 
-#' cases_averted_plot_option2b <- ggplot(data = cases_averted_df_mod_strategy,
-#'                                       aes(x = reorder(strategy, cases_averted),
-#'                                           y = cases_averted/1000
-#'                                       )
-#' ) + 
-#'   geom_col(aes(fill = cold_chain,
-#'                linetype = mt_equip_type),
-#'            color = 'black',
-#'            size = 1,
-#'            position = position_dodge2(preserve = 'single')
-#'   ) + 
-#'   scale_linetype_manual(name = 'Mobile team equipment',
-#'                         values = c('solid', 'twodash')) +
-#'   scale_fill_brewer(palette = 'Set3', 
-#'                     name = 'Cold chain option', 
-#'                     breaks = c('cc', 
-#'                                'no_cc', 
-#'                                'part_cc'),
-#'                     labels = c('Cold chain' , 
-#'                                'Outside cold chain', 
-#'                                'Partial cold chain' )) + 
-#'   scale_x_discrete(breaks = cases_averted_df_mod_strategy$strategy, 
-#'                    labels = cases_averted_df_mod_strategy$vial_type) +
-#'   guides(linetype = guide_legend(override.aes = list(fill = NA, col = 'black'))) +
-#'   labs(#title = 'Cases averted by mobile team equipment, vial type, and cold chain decision',
-#'        x = 'Vial type choice', 
-#'        y = 'Cases averted (thousands)') +
-#'   ggpubr::font('xy.text', face = 'plain') +
-#'   ggpubr::font('xy.title', face = 'plain') +
-#'   ggpubr::theme_pubr(legend = 'right', base_size = 18) +
-#'   NULL
-#' 
-#' plot(cases_averted_plot_option2b)
-#' 
-#' 
-#' #' option 3: Group by cold chain choice, sort the cases averted within each group from
-#' #' lowest to highest. Identify each bar by cold chain, vial type, and mobile team
-#' #' equipment type
-#' 
-#' 
-
-#' cases_averted_plot_option3 <- cases_averted_df_cc_mod %>% 
-#'   group_by(cold_chain) %>% 
-#'   arrange(cases_averted, .by_group = T) %>% 
-#'   ggplot(aes(group = cold_chain)) + 
-#'   geom_col(aes(x = cold_chain, 
-#'                y = cases_averted/1000, 
-#'                fill = cold_chain,
-#'                linetype = mt_equip_type, 
-#'                color = vial_type),
-#'            size = 1,
-#'            position = position_dodge2(preserve = 'single')
-#'   ) + 
-#'   scale_linetype_manual(name = 'Mobile team equipment',
-#'                         values = c('solid', 'twodash')) +
-#'   scale_fill_manual(name = 'Cold chain option', 
-#'                     breaks = c('cc', 
-#'                                'no_cc', 
-#'                                'part_cc'),
-#'                     labels = c('Cold chain' , 
-#'                                'Outside cold chain', 
-#'                                'Partial cold chain' ),
-#'                     values = c('cc' = 'seagreen1', 
-#'                                'no_cc' = 'dodgerblue', 
-#'                                'part_cc' = 'plum1')) + 
-#'   scale_color_manual(name = 'Vial type', 
-#'                      breaks = c('dose10', 
-#'                                  'monodose', 
-#'                                  'dose10 + monodose'),
-#'                       values = c('dose10' = 'seagreen4', 
-#'                                 'monodose' = 'dodgerblue4', 
-#'                                 'dose10 + monodose' = 'plum4')) +
-#'   scale_x_discrete(breaks = cases_averted_df_cc_mod$cold_chain, 
-#'                    labels = ifelse(cases_averted_df_cc_mod$cold_chain == 'cc', 
-#'                                    'Full cold chain',
-#'                                    ifelse(cases_averted_df_cc_mod$cold_chain == 'part_cc',
-#'                                           'Part cold chain', 'Outside cold chain'))) +
-#'   guides(linetype = guide_legend(override.aes = list(fill = NA, col = 'black')),
-#'          color = guide_legend(override.aes = list(fill = NA))) +
-#'   labs(#title = 'Cases averted by mobile team equipment, vial type, and cold chain decision',
-#'        x = 'Strategy', 
-#'        y = 'Cases averted (thousands)') +
-#'   ggpubr::font('xy.text', face = 'plain') +
-#'   ggpubr::font('xy.title', face = 'plain') +
-#'   ggpubr::theme_pubr(legend = 'right', base_size = 18) +
-#'   NULL
-#' 
-#' plot(cases_averted_plot_option3)
+#        )
 
 
 
