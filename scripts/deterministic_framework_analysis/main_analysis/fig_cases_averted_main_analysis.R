b0857235c142b0df73b6091ac3681c9239f93f2c
James Azam <jamesazam@sun.ac.za>
2022-01-20 12:28:49 +0200

    comment out the other cases averted plot

--- scripts/deterministic_framework_analysis/main_analysis/fig_cases_averted_main_analysis.R
+++ scripts/deterministic_framework_analysis/main_analysis/fig_cases_averted_main_analysis.R
@@ -167,104 +167,104 @@ ggsave(plot = relative_cases_averted_lollipop_plot,
        )
 
 #' Cases averted (here, the reference is the no orv counterfactual)
-cases_averted_lollipop_plot <- cases_averted_df_cc_mod %>% 
-  arrange(cases_averted, .by_group = T) %>% 
-  ggplot(aes(group = cold_chain)) + 
-  geom_linerange(aes(x = order(cold_chain, cases_averted), 
-                     ymin = 0, 
-                     ymax = ifelse(cases_averted == 0, 
-                                   0, 
-                                   cases_averted/1000-1
-                                   ),
-                     color = cold_chain
-                     ),
-                 size = 1.5
-                 ) +
-  geom_point(aes(x = order(cold_chain, cases_averted),
-                 y = cases_averted/1000,
-                 shape = mt_equip_type,
-                 fill = vial_type
-  ),
-  size = 6,
-  stroke = 1.5
-  ) +
-  scale_shape_manual(name = 'Mobile team equipment',
-                     values = c(21, 24),
-                     labels = c('rcw25' = 'RCW25',
-                                'vaxCarr' = 'Vaccine carrier')
-  ) +
-  scale_color_manual(name = 'Cold chain option', 
-                     breaks = c('cc', 
-                                'no_cc', 
-                                'part_cc'),
-                     labels = c('Cold chain' , 
-                                'Outside cold chain', 
-                                'Partial cold chain' ),
-                     values = c('cc' = '#00AFBB', 
-                                'no_cc' = '#FC4E07', 
-                                'part_cc' = '#E7B800')
-  ) + 
-  scale_fill_manual(name = 'Vial type',
-                    breaks = c('dose10', 
-                               'monodose',
-                               'dose10 + monodose'),
-                    values = c('dose10' = NA, 
-                               'monodose' = NA,
-                               'dose10 + monodose' = NA),
-                    labels = c('dose10' = '10-dose', 
-                               'monodose' = 'Monodose',
-                               'dose10 + monodose' = '10-dose & Monodose')
-  ) +
-  scale_x_discrete(breaks = cases_averted_df_cc_mod$cold_chain,
-                   labels = ifelse(cases_averted_df_cc_mod$cold_chain == 'cc',
-                                   'Full cold chain',
-                                   ifelse(cases_averted_df_cc_mod$cold_chain == 'part_cc',
-                                          'Part cold chain', 'Outside cold chain')
-                   )
-  ) +
-  scale_y_continuous(breaks = round(seq(0, 
-                                        max(cases_averted_df_cc_mod$cases_averted)/1000, 
-                                        15
-                                        ), 1
-                                    ),
-  labels = round(seq(0, 
-                     max(cases_averted_df_cc_mod$cases_averted)/1000, 
-                     15), 1
-                 )
-  ) +
-  guides(shape = guide_legend(override.aes = list(size = 6, 
-                                                  stroke = 1.2
-                                                  )
-                              ), 
-  fill = guide_legend(override.aes = list(size = 6, 
-                                          color = 'black', 
-                                          fill = 'white', 
-                                          shape = 22)
-                      )
-  ) +
-  facet_grid(~ cold_chain, 
-             scales = 'free_x', 
-             switch = 'x', 
-             labeller = labeller(cold_chain = c(cc = 'Full cold chain', 
-                                                part_cc = 'Partial cold chain', 
-                                                no_cc = 'Outside cold chain')
-             )
-  ) +
-  labs(x = 'Strategy', 
-       y = 'Cases averted (thousands)',
-       fill = 'Vial type') + 
-  theme_minimal(base_size = 18) +
-  NULL
-
-plot(cases_averted_lollipop_plot)
-
-#save plot
-ggsave(plot = cases_averted_lollipop_plot,
-       filename = './figures/deterministic_framework_analysis_figures/main_analysis/fig5_cases_averted_main_analysis_lollipop_plot.jpg',
-       width = 23.76,
-       height = 17.86,
-       units = 'cm'
-       )
+# cases_averted_lollipop_plot <- cases_averted_df_cc_mod %>% 
+#   arrange(cases_averted, .by_group = T) %>% 
+#   ggplot(aes(group = cold_chain)) + 
+#   geom_linerange(aes(x = order(cold_chain, cases_averted), 
+#                      ymin = 0, 
+#                      ymax = ifelse(cases_averted == 0, 
+#                                    0, 
+#                                    cases_averted/1000-1
+#                                    ),
+#                      color = cold_chain
+#                      ),
+#                  size = 1.5
+#                  ) +
+#   geom_point(aes(x = order(cold_chain, cases_averted),
+#                  y = cases_averted/1000,
+#                  shape = mt_equip_type,
+#                  fill = vial_type
+#   ),
+#   size = 6,
+#   stroke = 1.5
+#   ) +
+#   scale_shape_manual(name = 'Mobile team equipment',
+#                      values = c(21, 24),
+#                      labels = c('rcw25' = 'RCW25',
+#                                 'vaxCarr' = 'Vaccine carrier')
+#   ) +
+#   scale_color_manual(name = 'Cold chain option', 
+#                      breaks = c('cc', 
+#                                 'no_cc', 
+#                                 'part_cc'),
+#                      labels = c('Cold chain' , 
+#                                 'Outside cold chain', 
+#                                 'Partial cold chain' ),
+#                      values = c('cc' = '#00AFBB', 
+#                                 'no_cc' = '#FC4E07', 
+#                                 'part_cc' = '#E7B800')
+#   ) + 
+#   scale_fill_manual(name = 'Vial type',
+#                     breaks = c('dose10', 
+#                                'monodose',
+#                                'dose10 + monodose'),
+#                     values = c('dose10' = NA, 
+#                                'monodose' = NA,
+#                                'dose10 + monodose' = NA),
+#                     labels = c('dose10' = '10-dose', 
+#                                'monodose' = 'Monodose',
+#                                'dose10 + monodose' = '10-dose & Monodose')
+#   ) +
+#   scale_x_discrete(breaks = cases_averted_df_cc_mod$cold_chain,
+#                    labels = ifelse(cases_averted_df_cc_mod$cold_chain == 'cc',
+#                                    'Full cold chain',
+#                                    ifelse(cases_averted_df_cc_mod$cold_chain == 'part_cc',
+#                                           'Part cold chain', 'Outside cold chain')
+#                    )
+#   ) +
+#   scale_y_continuous(breaks = round(seq(0, 
+#                                         max(cases_averted_df_cc_mod$cases_averted)/1000, 
+#                                         15
+#                                         ), 1
+#                                     ),
+#   labels = round(seq(0, 
+#                      max(cases_averted_df_cc_mod$cases_averted)/1000, 
+#                      15), 1
+#                  )
+#   ) +
+#   guides(shape = guide_legend(override.aes = list(size = 6, 
+#                                                   stroke = 1.2
+#                                                   )
+#                               ), 
+#   fill = guide_legend(override.aes = list(size = 6, 
+#                                           color = 'black', 
+#                                           fill = 'white', 
+#                                           shape = 22)
+#                       )
+#   ) +
+#   facet_grid(~ cold_chain, 
+#              scales = 'free_x', 
+#              switch = 'x', 
+#              labeller = labeller(cold_chain = c(cc = 'Full cold chain', 
+#                                                 part_cc = 'Partial cold chain', 
+#                                                 no_cc = 'Outside cold chain')
+#              )
+#   ) +
+#   labs(x = 'Strategy', 
+#        y = 'Cases averted (thousands)',
+#        fill = 'Vial type') + 
+#   theme_minimal(base_size = 18) +
+#   NULL
+# 
+# plot(cases_averted_lollipop_plot)
+# 
+# #save plot
+# ggsave(plot = cases_averted_lollipop_plot,
+#        filename = './figures/deterministic_framework_analysis_figures/main_analysis/fig5_cases_averted_main_analysis_lollipop_plot.jpg',
+#        width = 23.76,
+#        height = 17.86,
+#        units = 'cm'
+#        )
 
 
 
